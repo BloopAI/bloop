d78787b205aa6d5690239167b9fe2a649e9d3634
anastasiia <anastasiya1155@gmail.com>
2022-12-13 12:36:06 +0800

    check if Segment write key is present in the environment when starting vite

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  publicDir: '../../client/public',
-  define: {
-    __APP_SESSION__: (Math.random() * 100000).toString(),
-  },
+export default defineConfig(({ mode }) => {
+  const envVars = loadEnv(mode, process.cwd());
+  if (mode === 'production' && !envVars?.VITE_SEGMENT_WRITE_KEY_PROD) {
+    throw new Error('Segment key must be present in .env file');
+  }
+  return {
+    plugins: [react()],
+    publicDir: '../../client/public',
+    define: {
+      __APP_SESSION__: (Math.random() * 100000).toString(),
+    },
+  };
 });
