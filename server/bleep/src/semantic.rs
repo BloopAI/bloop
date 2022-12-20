7f1c002e25410fbcaa2f197ccb3e2275cdf49369
rsdy <p@symmetree.dev>
2022-12-20 14:56:52 +0000

    Extract embedding

--- server/bleep/src/semantic.rs
+++ server/bleep/src/semantic.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 
+use anyhow::Result;
 use maplit::hashmap;
 use ndarray_linalg::Norm;
 use qdrant_client::{
@@ -56,6 +57,46 @@ impl Semantic {
         }
     }
 
+    pub fn embed(&self, chunk: &str) -> Result<Vec<f32>> {
+        let tokenizer_output = self.tokenizer.encode(chunk, true).unwrap();
+
+        let input_ids = tokenizer_output.get_ids();
+        let attention_mask = tokenizer_output.get_attention_mask();
+        let token_type_ids = tokenizer_output.get_type_ids();
+
+        let length = input_ids.len();
+
+        let input_ids: Tensor = tract_ndarray::Array2::from_shape_vec(
+            (1, length),
+            input_ids.iter().map(|&x| x as i64).collect(),
+        )?
+        .into();
+
+        let attention_mask: Tensor = tract_ndarray::Array2::from_shape_vec(
+            (1, length),
+            attention_mask.iter().map(|&x| x as i64).collect(),
+        )?
+        .into();
+
+        let token_type_ids: Tensor = tract_ndarray::Array2::from_shape_vec(
+            (1, length),
+            token_type_ids.iter().map(|&x| x as i64).collect(),
+        )?
+        .into();
+
+        let outputs = self.onnx.run(tvec!(
+            input_ids.into(),
+            attention_mask.into(),
+            token_type_ids.into()
+        ))?;
+
+        let logits = outputs[0].to_array_view::<f32>()?;
+        let logits = logits.slice(tract_ndarray::s![.., 0, ..]);
+        let norm = logits.norm();
+
+        Ok((logits.to_owned() / norm).as_slice().unwrap().to_vec())
+    }
+
     #[tracing::instrument(skip(self, buffer))]
     pub async fn insert_points_for_buffer(&self, file_path: &Path, buffer: &str, lang_str: &str) {
         let chunks = chunk::tree_sitter(&buffer, lang_str).unwrap_or_else(|e| {
@@ -74,81 +115,23 @@ impl Semantic {
                         .then_some("(big chunk)")
                         .unwrap_or_default()
                 );
-                let tokenizer_output = self.tokenizer.encode(chunk, true).unwrap();
 
-                let input_ids = tokenizer_output.get_ids();
-                let attention_mask = tokenizer_output.get_attention_mask();
-                let token_type_ids = tokenizer_output.get_type_ids();
-
-                let length = input_ids.len();
-
-                let input_ids: Tensor = tract_ndarray::Array2::from_shape_vec(
-                    (1, length),
-                    input_ids.iter().map(|&x| x as i64).collect(),
-                )
-                .map_err(|e| {
-                    tracing::error!("{e}");
-                    e
-                })
-                .ok()?
-                .into();
-                let attention_mask: Tensor = tract_ndarray::Array2::from_shape_vec(
-                    (1, length),
-                    attention_mask.iter().map(|&x| x as i64).collect(),
-                )
-                .map_err(|e| {
-                    tracing::error!("{e}");
-                    e
-                })
-                .ok()?
-                .into();
-                let token_type_ids: Tensor = tract_ndarray::Array2::from_shape_vec(
-                    (1, length),
-                    token_type_ids.iter().map(|&x| x as i64).collect(),
-                )
-                .map_err(|e| {
-                    tracing::error!("{e}");
-                    e
-                })
-                .ok()?
-                .into();
-
-                let outputs = self
-                    .onnx
-                    .run(tvec!(
-                        input_ids.into(),
-                        attention_mask.into(),
-                        token_type_ids.into()
-                    ))
-                    .map_err(|e| {
-                        tracing::error!("{e}");
-                        e
-                    })
-                    .ok()?;
-
-                let logits = outputs[0]
-                    .to_array_view::<f32>()
-                    .map_err(|e| {
-                        tracing::error!("{e}");
-                        e
-                    })
-                    .ok()?;
-
-                let logits = logits.slice(tract_ndarray::s![.., 0, ..]);
-
-                let norm = logits.to_owned().norm();
-
-                let data = logits.to_owned() / norm;
-                Some(PointStruct {
-                    id: Some(PointId::from(uuid::Uuid::new_v4().to_string())),
-                    vectors: Some(data.as_slice().unwrap().to_vec().into()),
-                    payload: hashmap! {
-                        "lang".into() => lang_str.to_ascii_lowercase().into(),
-                        "file".into() => file_path.to_string_lossy().as_ref().into(),
-                        "snippet".into() => chunk.into(),
-                    },
-                    ..Default::default()
-                })
+                match self.embed(&chunk) {
+                    Ok(ok) => Some(PointStruct {
+                        id: Some(PointId::from(uuid::Uuid::new_v4().to_string())),
+                        vectors: Some(ok.into()),
+                        payload: hashmap! {
+                            "lang".into() => lang_str.to_ascii_lowercase().into(),
+                            "file".into() => file_path.to_string_lossy().as_ref().into(),
+                            "snippet".into() => chunk.into(),
+                        },
+                        ..Default::default()
+                    }),
+                    Err(err) => {
+                        trace!(?err, "embedding failed");
+                        None
+                    }
+                }
             })
             .collect::<Vec<_>>();
 
