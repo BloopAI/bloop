956d3ab7e013f90093a95214510853f69ae99fd1
Akshay <nerdy@peppe.rs>
2022-12-21 19:12:31 +0530

    fix filter

--- server/bleep/src/semantic.rs
+++ server/bleep/src/semantic.rs
@@ -124,7 +124,7 @@ impl Semantic {
         debug!("found {} chunks", chunks.len());
         let datapoints = chunks
             .par_iter()
-            .filter(|chunk| chunk.len() < 50) // small chunks tend to skew results
+            .filter(|chunk| chunk.len() > 50) // small chunks tend to skew results
             .filter_map(|&chunk| {
                 debug!(
                     "chunk size: {}b {}",
