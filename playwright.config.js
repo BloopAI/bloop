28196c296dd8eba292c3e5777454c11eeafe4ef3
Pavel Yemelianov <ipauler@gmail.com>
2023-01-09 11:15:16 +0100

    Update tests config

--- playwright.config.js
+++ playwright.config.js
@@ -57,6 +57,10 @@ const config = {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
