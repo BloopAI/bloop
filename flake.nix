3a14ab2944c134a2564a3490559bdc145ef8b4cd
rsdy <p@symmetree.dev>
2023-02-15 14:21:41 +0000

    Run tests with nix

--- flake.nix
+++ flake.nix
@@ -26,51 +26,53 @@
 
         # Get a specific rust version
         rust = pkgs.rust-bin.stable.latest.default;
-      in {
-        devShell = pkgs.mkShell {
-          shellHook = ''
-            pnpm install >&2
-          '';
-          buildInputs = with pkgs;
-            ([
-              rust
-              sccache
-              git-lfs
-              stdenv
-              libclang
-              clang
-              rustup
-              nodePackages.pnpm
-              pkg-config
-              openssl
-              glib.dev
-              cmake
-              python3
-              protobuf
-              automake
-              autoconf
-              rocksdb
-              universal-ctags
-            ] ++ lib.optionals pkgs.stdenv.isLinux [
-              dbus.dev
-              libsoup.dev
-              gtk3.dev
-              webkitgtk
-              dmidecode
-              appimage-run
-              appimagekit
-            ] ++ lib.optionals pkgs.stdenv.isDarwin [
-              darwin.apple_sdk.frameworks.Carbon
-              darwin.apple_sdk.frameworks.WebKit
-              darwin.apple_sdk.frameworks.AppKit
-            ]);
+        buildDeps = with pkgs;
+          ([
+            rust
+            sccache
+            git-lfs
+            stdenv
+            libclang
+            clang
+            rustup
+            nodePackages.pnpm
+            pkg-config
+            openssl
+            glib.dev
+            cmake
+            python3
+            protobuf
+            automake
+            autoconf
+            rocksdb
+            universal-ctags
+          ] ++ lib.optionals pkgs.stdenv.isLinux [
+            dbus.dev
+            libsoup.dev
+            gtk3.dev
+            webkitgtk
+            dmidecode
+            appimage-run
+            appimagekit
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Carbon
+            darwin.apple_sdk.frameworks.WebKit
+            darwin.apple_sdk.frameworks.AppKit
+          ]);
 
-          SCCACHE_DIR = "/Users/user/Code/bloop/bloop/sccache";
+        buildEnv = {
           RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
           ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
           ROCKSDB_INCLUDE_DIR = "${pkgs.rocksdb}/include";
           LIBCLANG_PATH = "${libclang.lib}/lib";
         };
+      in rec {
+        devShell = pkgs.mkShell ({
+          shellHook = ''
+            pnpm install >&2
+          '';
+          buildInputs = buildDeps;
+        } // buildEnv);
 
         packages.my-ctags = pkgsStatic.universal-ctags.overrideAttrs (old: {
           nativeBuildInputs = old.nativeBuildInputs
