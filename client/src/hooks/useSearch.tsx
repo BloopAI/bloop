429dcfa6989db56e8ea8dba5a05dd9cf667101c9
anastasiia <anastasiya1155@gmail.com>
2023-02-16 09:59:16 +0000

    use only one eventSource connection to get the nl answer

--- client/src/hooks/useSearch.tsx
+++ client/src/hooks/useSearch.tsx
@@ -1,8 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
-import {
-  search as searchApiCall,
-  nlSearch as nlSearchApiCall,
-} from '../services/api';
+import { search as searchApiCall } from '../services/api';
 import { SearchContext } from '../context/searchContext';
 import { SearchType } from '../types/general';
 import { DeviceContext } from '../context/deviceContext';
@@ -10,9 +7,10 @@ import useAnalytics from './useAnalytics';
 
 interface Status<T> {
   loading: boolean;
-  error?: Error;
+  error?: Error | string;
   data?: T;
   query?: string;
+  nlAnswer?: string;
 }
 
 interface SearchResponse<T> extends Status<T> {
@@ -23,6 +21,8 @@ interface SearchResponse<T> extends Status<T> {
     forceSearchType?: SearchType,
   ) => void;
 }
+
+let prevEventSource: EventSource | undefined;
 
 export const useSearch = <T,>(
   query?: string,
@@ -50,19 +50,43 @@ export const useSearch = <T,>(
 
     switch (currentSearchType) {
       case SearchType.NL:
+        prevEventSource?.close();
         const eventSource = new EventSource(
           `${apiUrl.replace(
             'https:',
             '',
           )}/answer?q=${query}&user_id=${deviceId}`,
         );
+        prevEventSource = eventSource;
+        let i = 0;
         eventSource.onmessage = (ev) => {
-          const queryTime = Date.now() - startTime;
-          setLastQueryTime(queryTime);
-          trackSearch(queryTime);
-          setStatus({ loading: false, data: JSON.parse(ev.data), query });
-          // close connection after first message containing JSON, the rest will be accepted in Answer comp
-          eventSource.close();
+          if (ev.data === '[DONE]') {
+            eventSource.close();
+            prevEventSource = undefined;
+          } else {
+            const newData = JSON.parse(ev.data);
+
+            if (i === 0) {
+              if (newData.Err) {
+                const queryTime = Date.now() - startTime;
+                setLastQueryTime(queryTime);
+                trackSearch(queryTime);
+                setStatus((prev) => ({
+                  ...prev,
+                  loading: false,
+                  error: newData.Err,
+                }));
+              } else {
+                setStatus({ loading: false, data: JSON.parse(ev.data), query });
+              }
+            } else {
+              setStatus((prev) => ({
+                ...prev,
+                nlAnswer: (prev.nlAnswer || '') + newData.Ok,
+              }));
+            }
+            i++;
+          }
         };
         eventSource.onerror = (err) => {
           console.error('EventSource failed:', err);
