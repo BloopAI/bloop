b93c659ba949df846d1fb828ee3f0fc432f04a63
anastasiia <anastasiya1155@gmail.com>
2023-02-23 22:02:34 +0800

    show github login step if github credentials are not found and remote services are on

--- client/src/components/PageTemplate/index.tsx
+++ client/src/components/PageTemplate/index.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import NavBar from '../NavBar';
 import StatusBar from '../StatusBar';
+import { DeviceContext } from '../../context/deviceContext';
+import { AnalyticsContext } from '../../context/analyticsContext';
+import { UIContext } from '../../context/uiContext';
+import { STEP_KEY } from '../../pages/Home/Onboarding/RemoteServicesStep';
+import GithubConnectStep from '../../pages/Home/Onboarding/GithubConnectStep';
+import SeparateOnboardingStep from '../SeparateOnboardingStep';
+import {
+  IS_ANALYTICS_ALLOWED_KEY,
+  savePlainToStorage,
+} from '../../services/storage';
+import Button from '../Button';
 
 type Props = {
   children: React.ReactNode;
@@ -8,8 +19,49 @@ type Props = {
 
 const mainContainerStyle = { height: 'calc(100vh - 8rem)' };
 const PageTemplate = ({ children }: Props) => {
+  const { isSelfServe } = useContext(DeviceContext);
+  const { isAnalyticsAllowed, setIsAnalyticsAllowed } =
+    useContext(AnalyticsContext);
+  const { isGithubConnected, setOnBoardingState } = useContext(UIContext);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (!isGithubConnected && isAnalyticsAllowed && !isSelfServe) {
+      setModalOpen(true);
+    }
+  }, []);
+
+  const saveOptIn = useCallback((optIn: boolean) => {
+    savePlainToStorage(IS_ANALYTICS_ALLOWED_KEY, optIn ? 'true' : 'false');
+    setOnBoardingState((prev) => ({
+      ...prev,
+      [STEP_KEY]: { hasOptedIn: optIn },
+    }));
+    setIsAnalyticsAllowed(optIn);
+  }, []);
+
   return (
     <div className="text-gray-200">
+      <SeparateOnboardingStep isVisible={isModalOpen} top={'5rem'}>
+        <GithubConnectStep
+          handleNext={() => {
+            setModalOpen(false);
+            saveOptIn(true);
+          }}
+          description="We detected an issue with your GitHub credentials, please re-authenticate with GitHub again to enable remote services. Remote services are required to use natural language search. Opting out will disable natural language search."
+          secondaryCTA={
+            <Button
+              onClick={() => {
+                setModalOpen(false);
+                saveOptIn(false);
+              }}
+              variant="secondary"
+            >
+              Opt-Out of Remote Services
+            </Button>
+          }
+        />
+      </SeparateOnboardingStep>
       <NavBar userSigned />
       <div
         className="flex my-16 w-screen overflow-hidden relative z-10"
