6a107c70865b78bc05f16dcfa02f8ceb6d9377fe
Anastasiia Solop <35258279+anastasiya1155@users.noreply.github.com>
2023-03-01 17:47:33 +0700

    Release 0.2.3 (#220)
    * improve search analytics
    
    * prevent select all override if target is input
    
    * bump version

--- client/src/hooks/useAnalytics.ts
+++ client/src/hooks/useAnalytics.ts
@@ -3,9 +3,11 @@ import * as analytics from 'rudder-sdk-js';
 
 const useAnalytics = () => {
   const trackSearch = useCallback(
-    (queryTime: number) => {
+    (queryTime: number, query: string, searchId?: string) => {
       analytics?.track('Search', {
         queryTime,
+        query,
+        searchId,
       });
     },
     [analytics],
