521f26bcc4e8a688c37c1cae0c62d41fbf0c8d41
rsdy <p@symmetree.dev>
2023-03-06 14:42:12 +0000

    Hook in git-lfs

--- apps/desktop/build.sh
+++ apps/desktop/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+git lfs install
+git lfs pull
+
 if [ -z "$ORT_LIB_LOCATION" ]; then
     export ORT_LIB_LOCATION=$(readlink -f $(dirname $(readlink -f  $0))/../../lib/$(rustc -vV |awk '/host:/ { print $2 }')/onnxruntime)
 fi
