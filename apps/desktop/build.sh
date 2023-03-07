df4dbf5d59beb6406e9fcf1182b04464198e4293
rsdy <rsdy@users.noreply.github.com>
2023-03-07 17:13:38 +0000

    Update README & get rid of gnuisms (#236)

--- apps/desktop/build.sh
+++ apps/desktop/build.sh
@@ -4,7 +4,7 @@ git lfs install
 git lfs pull
 
 if [ -z "$ORT_LIB_LOCATION" ]; then
-    export ORT_LIB_LOCATION=$(readlink -f $(dirname $(readlink -f  $0))/../../lib/$(rustc -vV |awk '/host:/ { print $2 }')/onnxruntime)
+    export ORT_LIB_LOCATION=$(realpath $(dirname $(realpath $0))/../../lib/$(rustc -vV |awk '/host:/ { print $2 }')/onnxruntime)
 fi
 
 echo $ORT_LIB_LOCATION
