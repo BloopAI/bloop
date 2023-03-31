70ce4a30e1ee4e801c44726911c338dc5787aa02
anastasiia <anastasiya1155@gmail.com>
2023-03-31 11:19:03 +0300

    put traffic lights into navbar

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -12,9 +12,10 @@ const NavBar = ({ userSigned, isSkeleton }: Props) => {
   const { navigateHome } = useAppNavigation();
   return (
     <div
-      className={`h-16 flex items-center gap-6 px-8 bg-gray-800/75 fixed top-0 left-0 right-0 z-30 ${
+      className={`h-16 flex items-center gap-6 pr-8 pl-24 bg-gray-800/75 fixed top-0 left-0 right-0 z-30 ${
         userSigned ? '' : 'justify-between'
       } border-b border-gray-700 backdrop-blur-8`}
+      data-tauri-drag-region={true}
     >
       {!isSkeleton && (
         <span className="text-gray-50">
