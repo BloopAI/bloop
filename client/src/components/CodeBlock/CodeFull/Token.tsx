988723dc2c2302fd11edcf9ae587fdcec883337d
anastasiia <anastasiya1155@gmail.com>
2023-03-31 15:31:07 +0300

    use sse on fe

--- client/src/components/CodeBlock/CodeFull/Token.tsx
+++ client/src/components/CodeBlock/CodeFull/Token.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
+import Tippy, { TippyProps } from '@tippyjs/react';
 import TooltipCode from '../../TooltipCode';
 import CodeToken from '../Code/CodeToken';
 import { Token as TokenType } from '../../../types/prism';
@@ -30,6 +31,8 @@ const tokenHoverable = (tokenPosition: Range, ranges: Range[]) => {
   }
 };
 
+let prevEventSource: EventSource;
+
 const Token = ({
   language,
   token,
@@ -42,6 +45,7 @@ const Token = ({
   const [hoverableRange, setHoverableRange] = useState(
     tokenHoverable(token.byteRange, lineHoverRanges),
   );
+  const [info, setInfo] = useState('');
 
   const [tokenInfo, setTokenInfo] = useState<TokenInfo>({
     definitions: [],
@@ -51,18 +55,44 @@ const Token = ({
   const getHoverableContent = useCallback(
     (hoverableRange: Range) => {
       if (hoverableRange && relativePath) {
-        getTokenInfo(
-          relativePath,
-          repoPath,
-          hoverableRange.start,
-          hoverableRange.end,
-        ).then((data) => {
-          setTokenInfo(mapTokenInfoData(data));
-        });
+        // getTokenInfo(
+        //   relativePath,
+        //   repoPath,
+        //   hoverableRange.start,
+        //   hoverableRange.end,
+        // ).then((data) => {
+        //   setTokenInfo(mapTokenInfoData(data));
+        // });
+        prevEventSource?.close();
+        prevEventSource = new EventSource(
+          `http://localhost:7878/api/trace?relative_path=${encodeURIComponent(
+            relativePath,
+          )}&repo_ref=${encodeURIComponent(repoPath)}&start=${
+            hoverableRange.start
+          }&end=${hoverableRange.end}`,
+        );
+        prevEventSource.onmessage = (resp) => {
+          console.log(resp.data);
+          if (resp.data === '[DONE]') {
+            prevEventSource.close();
+            return;
+          }
+          const data = JSON.parse(resp.data);
+          if (data.Ok) {
+            setInfo((prev) => prev + data.Ok);
+          }
+        };
+        prevEventSource.onerror = console.log;
       }
     },
     [relativePath],
   );
+
+  useEffect(() => {
+    return () => {
+      prevEventSource?.close();
+    };
+  }, []);
 
   useEffect(() => {
     setHoverableRange(tokenHoverable(token.byteRange, lineHoverRanges));
@@ -86,16 +116,22 @@ const Token = ({
   );
 
   return hoverableRange ? (
-    <TooltipCode
-      language={language}
-      data={tokenInfo}
-      position={'left'}
-      onHover={onHover}
-      repoName={repoName}
-      onRefDefClick={handleRefsDefsClick}
+    <Tippy
+      interactive
+      trigger="click"
+      appendTo={(ref) => ref.ownerDocument.body}
+      onShow={onHover}
+      hideOnClick="toggle"
+      render={() => (
+        <div className="max-w-[300px] bg-gray-900 p-4 border border-gray-700 caption">
+          <pre className="whitespace-pre-wrap">{info}</pre>
+        </div>
+      )}
     >
-      <CodeToken token={token} />
-    </TooltipCode>
+      <span className={'cursor-pointer'}>
+        <CodeToken token={token} />
+      </span>
+    </Tippy>
   ) : (
     <CodeToken token={token} />
   );
