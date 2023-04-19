d9267c3ccb7ef76692c49ce0c92b620f9b1d52b7
anastasiia <anastasiya1155@gmail.com>
2023-04-19 13:38:48 +0200

    show code results from answers

--- client/src/pages/ConversationResult/index.tsx
+++ client/src/pages/ConversationResult/index.tsx
@@ -1,22 +1,40 @@
-import React, { useMemo } from 'react';
-import { conversationsCache } from '../../services/cache';
+import React, { useContext, useMemo } from 'react';
 import PageHeader from '../../components/ResultsPageHeader';
+import { ChatContext } from '../../context/chatContext';
+import { ChatMessageServer } from '../../types/general';
 import NewCode from './NewCode';
+import DiffCode from './DiffCode';
 
 type Props = {
-  recordId: string;
+  recordId: number;
 };
 
 const ConversationResult = ({ recordId }: Props) => {
-  const data = useMemo(() => conversationsCache[recordId], [recordId]);
+  const { conversation } = useContext(ChatContext);
+  const data = useMemo(
+    () =>
+      (conversation[recordId] as ChatMessageServer)?.fullAnswer?.filter(
+        (p) => p[0] !== 'cite' && p[0] !== 'con',
+      ) || [],
+    [conversation[recordId]],
+  );
   return (
     <div className="p-8 flex-1 overflow-x-auto mx-auto max-w-6.5xl box-content">
       <PageHeader
-        resultsNumber={data.data?.length}
+        resultsNumber={data?.length}
         showCollapseControls={false}
         loading={false}
       />
-      {data.type === 'new-code' && <NewCode {...data.data[0]} />}
+      <div className="flex flex-col gap-4">
+        {data.map((d, i) => {
+          if (d[0] === 'new' && d[2]) {
+            return <NewCode code={d[2]} language={d[1]} key={i} />;
+          } else if (d[0] === 'mod' && d[2]?.hunks) {
+            return <DiffCode data={d[2]} key={i} />;
+          }
+          return null;
+        })}
+      </div>
     </div>
   );
 };
