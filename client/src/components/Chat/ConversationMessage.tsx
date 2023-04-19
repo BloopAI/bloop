fb11bf0defafffeed15cc4691b04865d79b9ad0f
anastasiia <anastasiya1155@gmail.com>
2023-04-19 11:56:03 +0200

    use /answer endpoint

--- client/src/components/Chat/ConversationMessage.tsx
+++ client/src/components/Chat/ConversationMessage.tsx
@@ -2,15 +2,17 @@ import React, { useCallback, useState } from 'react';
 import { useRive } from '@rive-app/react-canvas';
 import { QuillIcon } from '../../icons';
 import Button from '../Button';
-import useAnalytics from '../../hooks/useAnalytics';
-import { saveUpvote } from '../../services/api';
+// import useAnalytics from '../../hooks/useAnalytics';
+// import { saveUpvote } from '../../services/api';
+import { ChatMessageAuthor } from '../../types/general';
 
 type Props = {
-  author: 'user' | 'server';
+  author: ChatMessageAuthor;
   message: string;
+  isHistory: boolean;
 };
 
-const ConversationMessage = ({ author, message }: Props) => {
+const ConversationMessage = ({ author, message, isHistory }: Props) => {
   const [isUpvote, setIsUpvote] = useState(false);
   const [isDownvote, setIsDownvote] = useState(false);
   const RiveUpvote = useRive({
@@ -56,11 +58,13 @@ const ConversationMessage = ({ author, message }: Props) => {
 
   return (
     <div
-      className={`relative group ${
-        author === 'user' ? 'bg-gray-800' : 'bg-gray-700'
+      className={`relative group-custom ${
+        author === ChatMessageAuthor.User || !isHistory
+          ? 'bg-gray-800'
+          : 'bg-gray-700'
       } flex items-center p-4 gap-3 border border-gray-700 rounded-lg`}
     >
-      {author === 'user' && (
+      {author === ChatMessageAuthor.User && (
         <div className="relative">
           <div className="w-6 h-6 rounded-full bg-gray-500" />
           <div className="absolute -bottom-1 -right-1 w-4 h-3 bg-gray-600 box-content border-2 border-gray-800 text-white rounded-full flex items-center justify-center">
@@ -71,9 +75,9 @@ const ConversationMessage = ({ author, message }: Props) => {
         </div>
       )}
       <pre className="body-s text-gray-200 whitespace-pre-wrap">{message}</pre>
-      {author === 'server' && !!message && (
+      {author === ChatMessageAuthor.Server && !!message && (
         <div
-          className={`absolute top-2 right-2 flex items-center gap-1 p-1 bg-gray-900/75 backdrop-blur-4 rounded-md opacity-0 group-hover:opacity-100 transition-opacity`}
+          className={`absolute top-2 right-2 flex items-center gap-1 p-1 bg-gray-900/75 backdrop-blur-4 rounded-md opacity-0 group-custom-hover:opacity-100 transition-opacity`}
         >
           <Button
             variant={'tertiary'}
