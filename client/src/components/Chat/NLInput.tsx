8398aea85b4d071e50480bff1dc81352479e66bd
anastasiia <anastasiya1155@gmail.com>
2023-04-21 14:11:51 +0200

    new response format, annotations, auto-growing input, stop generating btn

--- client/src/components/Chat/NLInput.tsx
+++ client/src/components/Chat/NLInput.tsx
@@ -1,11 +1,15 @@
-import { ChangeEvent } from 'react';
-import { QuillIcon } from '../../icons';
+import React, { ChangeEvent, useEffect, useRef } from 'react';
+import { QuillIcon, SendIcon } from '../../icons';
+import ClearButton from '../ClearButton';
+import Tooltip from '../Tooltip';
 
 type Props = {
   id?: string;
   value?: string;
   placeholder?: string;
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  isStoppable?: boolean;
+  onStop?: () => void;
+  onChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 };
 
 const NLInput = ({
@@ -13,24 +17,57 @@ const NLInput = ({
   value,
   onChange,
   placeholder = 'Anything I can help you with?',
+  isStoppable,
+  onStop,
 }: Props) => {
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  useEffect(() => {
+    if (inputRef.current) {
+      // We need to reset the height momentarily to get the correct scrollHeight for the textarea
+      inputRef.current.style.height = '56px';
+      const scrollHeight = inputRef.current.scrollHeight;
+
+      // We then set the height directly, outside of the render loop
+      // Trying to set this with state or a ref will product an incorrect value.
+      inputRef.current.style.height = Math.min(scrollHeight, 300) + 'px';
+    }
+  }, [inputRef.current, value]);
   return (
     <div
-      className={`w-full flex items-center gap-2 bg-gray-800 rounded-lg disabled:border-transparent disabled:text-gray-500
-    border border-gray-700 focus-within:border-gray-600 hover:border-gray-600 pl-4
-    text-gray-400 focus-within:text-gray-100 hover:text-gray-100 transition-all ease-out duration-150`}
+      className={`w-full flex items-start gap-2 bg-gray-800 rounded-lg disabled:border-transparent disabled:text-gray-500
+    border border-gray-700 focus-within:border-gray-600 hover:border-gray-600 px-4
+    text-gray-400 focus-within:text-gray-100 hover:text-gray-100 transition-all ease-out duration-150 flex-grow-0`}
     >
-      <QuillIcon />
-      <input
-        className={`w-full p-4 pl-0 bg-transparent rounded-lg outline-none focus:outline-0 
-        placeholder:text-current transition-all ease-out duration-150`}
+      <span className="py-4">
+        <QuillIcon />
+      </span>
+      <textarea
+        className={`w-full py-4 bg-transparent rounded-lg outline-none focus:outline-0 resize-none
+        placeholder:text-current flex-grow-0`}
         placeholder={placeholder}
         id={id}
         value={value}
         onChange={onChange}
+        rows={1}
         autoComplete="off"
-        type="search"
+        spellCheck="false"
+        ref={inputRef}
       />
+      {isStoppable ? (
+        <div className="relative top-[18px]">
+          <Tooltip text={'Stop generating'} placement={'top-end'}>
+            <ClearButton onClick={onStop} />
+          </Tooltip>
+        </div>
+      ) : value ? (
+        <button type="submit" className="self-end py-3 text-primary-300">
+          <Tooltip text={'Submit'} placement={'top-end'}>
+            <SendIcon />
+          </Tooltip>
+        </button>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
--- client/src/pages/ConversationResult/DiffCode.tsx
+++ client/src/pages/ConversationResult/DiffCode.tsx
@@ -6,9 +6,10 @@ import { copyToClipboard } from '../../utils';
 import FileIcon from '../../components/FileIcon';
 import BreadcrumbsPath from '../../components/BreadcrumbsPath';
 import { FileTreeFileType } from '../../types';
+import { MessageResultModify } from '../../types/general';
 
 type Props = {
-  data: any;
+  data: MessageResultModify['Modify'];
 };
 
 const DiffCode = ({ data }: Props) => {
@@ -16,19 +17,19 @@ const DiffCode = ({ data }: Props) => {
     <div className="text-sm border border-gray-700 rounded-md">
       <div className="w-full bg-gray-800 py-1 px-3 border-b border-gray-700 select-none">
         <div className="flex items-center gap-2 max-w-[calc(100%-120px)] w-full h-11.5">
-          {data.oldFileName !== data.newFileName && '-'}
-          <FileIcon filename={data.oldFileName} />
+          {data.diff.old_file_name !== data.diff.new_file_name && '-'}
+          <FileIcon filename={data.diff.old_file_name} />
           <BreadcrumbsPath
-            path={data.oldFileName}
+            path={data.diff.old_file_name}
             repo={''}
             onClick={(path, type) => (type === FileTreeFileType.FILE ? {} : {})}
           />
         </div>
-        {data.oldFileName !== data.newFileName && (
+        {data.diff.old_file_name !== data.diff.new_file_name && (
           <div className="flex items-center gap-2 max-w-[calc(100%-120px)] w-full h-11.5">
-            + <FileIcon filename={data.newFileName} />
+            + <FileIcon filename={data.diff.new_file_name} />
             <BreadcrumbsPath
-              path={data.newFileName}
+              path={data.diff.new_file_name}
               repo={''}
               onClick={(path, type) =>
                 type === FileTreeFileType.FILE ? {} : {}
@@ -37,18 +38,20 @@ const DiffCode = ({ data }: Props) => {
           </div>
         )}
       </div>
-      {data.hunks?.[0]?.lines ? (
+      {data.diff.hunks?.[0]?.lines ? (
         <div className="relative overflow-auto py-4">
           <Code
-            lineStart={data.hunks?.[0]?.oldStart}
-            code={data.hunks?.[0]?.lines?.join('\n')}
+            lineStart={data.diff.hunks?.[0]?.old_start}
+            code={data.diff.hunks?.[0]?.lines?.join('\n')}
             language={'TSX'}
           />
           <div className="absolute top-4 right-4">
             <Button
               variant="secondary"
               size="small"
-              onClick={() => copyToClipboard(data.hunks[0].lines.join('\n'))}
+              onClick={() =>
+                copyToClipboard(data.diff.hunks[0].lines.join('\n'))
+              }
             >
               <Clipboard />
               Copy
