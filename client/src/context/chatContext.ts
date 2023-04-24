0b3bc6a17f0e53e2cb959a29c2df237d433e94c6
anastasiia <anastasiya1155@gmail.com>
2023-04-24 15:25:05 +0200

    open / close the app when answering

--- client/src/context/chatContext.ts
+++ client/src/context/chatContext.ts
@@ -4,9 +4,13 @@ import { ChatMessage } from '../types/general';
 type ContextType = {
   conversation: ChatMessage[];
   setConversation: Dispatch<SetStateAction<ChatMessage[]>>;
+  isChatOpen: boolean;
+  setChatOpen: Dispatch<SetStateAction<boolean>>;
 };
 
 export const ChatContext = createContext<ContextType>({
   conversation: [],
   setConversation: () => {},
+  isChatOpen: false,
+  setChatOpen: () => {},
 });
