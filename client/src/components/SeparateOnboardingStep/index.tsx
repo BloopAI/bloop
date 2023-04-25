4d9f76f722ba04f35ed24fb6ad208fa3d530bdb5
anastasiia <anastasiya1155@gmail.com>
2023-04-25 15:19:19 +0200

    new home page

--- client/src/components/SeparateOnboardingStep/index.tsx
+++ client/src/components/SeparateOnboardingStep/index.tsx
@@ -5,7 +5,6 @@ import { MODAL_SIDEBAR_APPEAR_ANIMATION } from '../../consts/animations';
 type Props = {
   isVisible: boolean;
   onClose?: () => void;
-  top?: string;
 };
 
 const backdropHidden = {
@@ -20,25 +19,26 @@ const backdropVisible = {
   '-webkit-backdrop-filter': 'blur(1px)',
 };
 
-const initialModalStyles = (top: string) => ({
-  top,
+const initialModalStyles = {
+  top: '50%',
   right: '50%',
-  transform: 'translate(50%, 1rem)',
+  transform: 'translate(50%, -45%)',
   opacity: 0,
-});
+  maxHeight: 600,
+};
 
-const modalAnimation = (top: string) => ({
-  top,
+const modalAnimation = {
+  top: '50%',
   right: '50%',
-  transform: 'translate(50%, 0%)',
+  transform: 'translate(50%, -50%)',
   opacity: 1,
-});
+  maxHeight: 600,
+};
 
 const SeparateOnboardingStep = ({
   onClose,
   children,
   isVisible,
-  top = '1rem',
 }: PropsWithChildren<Props>) => {
   return (
     <>
@@ -60,10 +60,10 @@ const SeparateOnboardingStep = ({
         {isVisible && (
           <motion.div
             className={`overflow-auto fixed flex flex-col rounded-md drop-shadow-light-bigger bg-gray-900 
-            border border-gray-700 bg-opacity-75 z-70 backdrop-blur-8 max-h-[calc(100%-3rem)]`}
-            animate={modalAnimation(top)}
-            initial={initialModalStyles(top)}
-            exit={initialModalStyles(top)}
+            border border-gray-800 bg-opacity-75 z-70 backdrop-blur-8`}
+            animate={modalAnimation}
+            initial={initialModalStyles}
+            exit={initialModalStyles}
             transition={MODAL_SIDEBAR_APPEAR_ANIMATION}
           >
             <div className="p-6 flex flex-col gap-8 w-99 relative flex-1 overflow-auto">
