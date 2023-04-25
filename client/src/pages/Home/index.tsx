1a42a0fda85e008c1d90687e8327500c7e2edea4
anastasiia <anastasiya1155@gmail.com>
2023-04-25 16:36:51 +0200

    add popup after syncing repo

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import * as Sentry from '@sentry/react';
 import ErrorFallback from '../../components/ErrorFallback';
+import LiteLoader from '../../components/Loaders/LiteLoader';
+import Button from '../../components/Button';
+import { CloseSign } from '../../icons';
 import AddRepos from './AddRepos';
 import ReposSection from './ReposSection';
 import AddRepoCard from './AddRepoCard';
 
 const HomePage = () => {
+  const [popupOpen, setPopupOpen] = useState(false);
   const [isAddReposOpen, setAddReposOpen] = useState<
     null | 'local' | 'github' | 'public'
   >(null);
@@ -22,10 +26,37 @@ const HomePage = () => {
       <ReposSection />
       <AddRepos
         addRepos={isAddReposOpen}
-        onClose={() => {
+        onClose={(isSubmitted) => {
+          if (isSubmitted) {
+            setPopupOpen(true);
+            setTimeout(() => setPopupOpen(false), 3000);
+          }
           setAddReposOpen(null);
         }}
       />
+      {popupOpen && (
+        <div
+          className={`fixed w-85 p-3 flex gap-3 bg-gray-800 border border-gray-700 shadow-dark left-8 bottom-24 z-40 text-primary-300`}
+        >
+          <LiteLoader />
+          <div className="flex flex-col gap-1">
+            <p className="body-s text-white">Syncing repository</p>
+            <p className="caption text-gray-400">
+              We are syncing your repository to bloop. This might take a couple
+              on minutes
+            </p>
+          </div>
+          <Button
+            variant="tertiary"
+            size="tiny"
+            onlyIcon
+            title="Close"
+            onClick={() => setPopupOpen(false)}
+          >
+            <CloseSign />
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
