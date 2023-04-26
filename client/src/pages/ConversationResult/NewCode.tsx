edd59f7e6dada50f352ed6ce4f11aecefdcabd1d
anastasiia <anastasiya1155@gmail.com>
2023-04-26 12:34:57 +0200

    fix copy button position in NewCode

--- client/src/pages/ConversationResult/NewCode.tsx
+++ client/src/pages/ConversationResult/NewCode.tsx
@@ -10,19 +10,23 @@ type Props = {
 
 const NewCode = ({ code, language }: Props) => {
   return (
-    <div className="text-sm p-4 border border-gray-700 rounded-md">
-      <div className="relative overflow-auto">
+    <div className="text-sm p-4 border border-gray-700 rounded-md relative">
+      <div className="overflow-auto">
         <Code showLines={false} code={code} language={language} />
-        <div className="absolute top-0 right-0">
-          <Button
-            variant="secondary"
-            size="small"
-            onClick={() => copyToClipboard(code)}
-          >
-            <Clipboard />
-            Copy
-          </Button>
-        </div>
+      </div>
+      <div
+        className={`absolute ${
+          code.split('\n').length > 1 ? 'top-4 right-4' : 'top-2.5 right-2.5'
+        }`}
+      >
+        <Button
+          variant="secondary"
+          size="small"
+          onClick={() => copyToClipboard(code)}
+        >
+          <Clipboard />
+          Copy
+        </Button>
       </div>
     </div>
   );
