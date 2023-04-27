aac60a4ab405ee2f1a963c9babef493dbae2469f
anastasiia <anastasiya1155@gmail.com>
2023-04-27 14:46:45 +0200

    minor ui fixes

--- client/src/components/Breadcrumbs/BreadcrumbsCollapsed.tsx
+++ client/src/components/Breadcrumbs/BreadcrumbsCollapsed.tsx
@@ -1,13 +1,27 @@
 import { useMemo, useState } from 'react';
 import ContextMenu from '../ContextMenu';
 import { MenuItemType } from '../../types/general';
+import { MoreHorizontal } from '../../icons';
 import { PathParts } from './index';
 
 type Props = {
   items: PathParts[];
+  type: 'link' | 'button';
 };
 
-const BreadcrumbsCollapsed = ({ items }: Props) => {
+const typeMap = {
+  link: {
+    default: 'bg-none text-gray-500 hover:text-sky-500 active:text-sky-500',
+    isHiddenClicked: 'text-gray-200 hover:text-sky-500 active:text-sky-500',
+  },
+  button: {
+    default:
+      'px-2 py-1 rounded-4 bg-gray-800 hover:bg-gray-700 text-gray-400 hover:text-gray-100',
+    isHiddenClicked: 'text-gray-100 bg-gray-700 px-2 py-1 rounded-4',
+  },
+};
+
+const BreadcrumbsCollapsed = ({ items, type }: Props) => {
   const [isHiddenClicked, setIsHiddenClicked] = useState(false);
   const contextMenuItems = useMemo(
     () =>
@@ -29,15 +43,17 @@ const BreadcrumbsCollapsed = ({ items }: Props) => {
       >
         <span>
           <button
-            className={`bg-none p-0 outline-0 outline-none focus:outline-none border-0 ${
-              isHiddenClicked ? 'text-gray-200' : 'text-gray-500'
-            } hover:text-sky-500 active:text-sky-500`}
+            className={`p-0 outline-0 outline-none focus:outline-none border-0 flex items-center ${
+              isHiddenClicked
+                ? typeMap[type].isHiddenClicked
+                : typeMap[type].default
+            }`}
             onMouseDown={(e) => {
               e.stopPropagation();
               setIsHiddenClicked(() => !isHiddenClicked);
             }}
           >
-            ...
+            <MoreHorizontal />
           </button>
         </span>
       </ContextMenu>
