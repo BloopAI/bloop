db11b686c6ef96fd5d15e4f064a12f3fd044ef41
calyptobai <111788964+calyptobai@users.noreply.github.com>
2023-04-27 02:06:10 -0400

    Implement tool-based answer module (#387)
    * Implement tool-based answer module
    
    Here, we utilize the LLM to answer questions by giving it several
    actions to select from. Each action operates on our data in some way, for
    example the `path` action will search for files. The responses to these
    actions build up knowledge for the LLM such that it can answer questions
    better.
    
    The `answer` module was entirely rewritten, and split up. A new
    `llm_gateway` module holds an interface to the "Answer API", and we
    also split prompts out into their own module.
    
    * if lexical path search returns no results, back off to semantic path search
    
    * implement structured answer format (#399)
    
    * Add new structured answer format
    
    Here, we introduce the new answer format, which requires partially
    parsing the JSON responses. These partially parsed responses are
    streamed as updates, after an attempt to rectify malformed JSON
    values.
    
    * implement rectify_object
    
    * fix rectify_number to operate on single digit numbers
    
    * add line numbers to citations, improve prompting
    
    * fix incorrect comma handling in rectify_object
    
    * change answer::endpoint from POST to GET
    
    ---------
    
    Co-authored-by: calyptobai <calyptobai@gmail.com>
    
    * integrate fuzzy path-search (#424)
    
    ---------
    
    Co-authored-by: Gabriel Gordon-Hall <ggordonhall@gmail.com>
    Co-authored-by: Akshay <nerdy@peppe.rs>

---
+++ server/bleep/src/webserver/answer/prompts.rs
@@ -0,0 +1,126 @@
+//! Various prompts passed to the LLM.
+
+pub const INITIAL_PROMPT: &str = r#"["ask","Hi there, what can I help you with?"]"#;
+
+pub const CONTINUE: &str = "Is there anything else I can help with?";
+
+pub const SYSTEM: &str = r#"You must adhere to the following rules at all times:
+1. Your job is to act as a decision maker answering user's questions about the codebase.
+2. Decide which ACTION should be taken next.
+3. Do not repeat an ACTION.
+4. Do not assume the structure of the codebase, or the existence of files or folders.
+5. Respond only in valid JSON format.
+6. If an ACTION does not provide new information to answer the question, try a different ACTION or change your search terms.
+7. Only reply to the user with the "answer" ACTION.
+8. Do not use the answer action without first making a search.
+9. Check all possible files for answers before answering. Gather all information necessary to write a comprehensive answer, before using the answer ACTION.
+10. If the user asks for two things at once, tell them that the query is too complicated and to politely ask their questions separately.
+11. The user's question is related to the codebase.
+12. You can only perform one action at a time.
+13. If a path has an alias, use the alias instead of the full path when using the path with an action.
+
+#####
+
+Below is a list of available ACTIONS:
+
+1. Search code using semantic search
+["code",STRING: SEARCH TERMS]
+Returns a list of paths and relevant code.
+
+2. Search file paths using exact text match
+["path",STRING: SEARCH TERMS]
+To list all files within a repo, leave the search terms blank.
+To find all files from a particular programming language, write a single file extension.
+To search for all files within a folder, write just the name of the folder.
+
+3. Read a file's contents
+["file",INT: §ALIAS]
+OR
+["file",STRING: PATH]
+Retrieve the contents of a single file.
+
+4. Check files for answer
+["check",STRING: QUESTION,INT[]: §ALIAS FOR EACH FILE]
+Check more than one file. Do not use this action if you are only checking one file.
+Do not check the same file more than once.
+
+5. State that you are ready to answer the question after absolutely all information has been gathered
+["answer",STRING: STANDALONE USER REQUEST]
+Signal that you are ready to answer the user's request. Do not write your response.
+Your STANDALONE USER REQUEST should be based on all of the previous conversation with the user.
+It should be possible to understand from this string alone what the user is asking for."#;
+
+pub fn file_explanation(question: &str, path: &str, code: &str) -> String {
+    format!(
+        r#"Here's the contents of the code file {path} in <code> tags:
+
+<code>
+{code}
+</code>
+
+The code is one file of many that can help answer a user query.
+
+The user's query is: {question}
+
+Answer in the following JSON format, identifying any relevant line ranges:
+[{{
+"start":int,
+"end":int,
+"answer":[natural language description]
+}}]
+
+If the user's query cannot be answered by the file do not answer, instead reply with "0".
+
+Do not repeat the question in your answer.
+Do not make any assumptions, your answer should only refer to insights taken from the code."#
+    )
+}
+
+pub fn final_explanation_prompt(context: &str, question: &str) -> String {
+    format!(
+        r#"{context}#####
+Above are several pieces of information that can help you answer a user query.
+
+The user's query is "{question}"
+
+Your answer should be in the following JSON format: a list of objects, where each object represents one instance of:
+
+1. citing a single file from the codebase (this object can appear multiple times, in the form of a JSON array)
+START LINE and END LINE should focus on the code mentioned in the COMMENT.
+
+[["cite",INT: §ALIAS, STRING: COMMENT, INT: START LINE, INT: END LINE],
+["cite",INT: §ALIAS, STRING: COMMENT, INT: START LINE, INT: END LINE]]
+
+2. write a new code file (this object can appear multiple times)
+Do not use this to demonstrate updating an existing file.
+
+["new",STRING: LANGUAGE,STRING: CODE]
+
+3. update the code in an existing file (this object can appear multiple times)
+This is the best way to demonstrate updating an existing file.
+Changes should be as small as possible.
+
+["mod",INT: §ALIAS,CHANGES]
+Where CHANGES is an array of changes in the format:
+{{
+  oldFileName: 'oldfile', newFileName: 'newfile',
+  oldHeader: 'header1', newHeader: 'header2',
+  hunks: [{{
+    oldStart: 1, oldLines: 3, newStart: 1, newLines: 3,
+    lines: [' line2', ' line3', '-line4', '+line5', '\\ No newline at end of file'],
+  }}]
+}}
+The 'lines' array should contain each line of code within the line range.
+If you are adding a line, put a '+' at the start of the line.
+If you are removing a line, put a '-' at the start of the line.
+
+4. conclusion (this object is mandatory and must appear once at the end)
+
+["con",STRING: COMMENT]
+
+Your response should be a JSON array of objects.
+Refer to directories by their full paths, surrounded by single backticks.
+
+#####"#
+    )
+}
