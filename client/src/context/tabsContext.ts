0b97261f0a944b350bd2d5fa61fa82ca7aa80fce
anastasiia <anastasiya1155@gmail.com>
2023-04-28 17:20:56 +0200

    fix getting file for GitHub repos

--- client/src/context/tabsContext.ts
+++ client/src/context/tabsContext.ts
@@ -4,13 +4,13 @@ import { UITabType } from '../types/general';
 type ContextType = {
   tabs: UITabType[];
   activeTab: string;
-  handleAddTab: (repoRef: string, repoName: string) => void;
+  handleAddTab: (repoRef: string, repoName: string, name: string) => void;
   handleRemoveTab: (t: string) => void;
   setActiveTab: (t: string) => void;
 };
 
 export const TabsContext = createContext<ContextType>({
-  tabs: [{ key: 'initial', name: 'Home' }],
+  tabs: [{ key: 'initial', name: 'Home', repoName: '' }],
   activeTab: 'initial',
   handleAddTab: () => {},
   handleRemoveTab: () => {},
