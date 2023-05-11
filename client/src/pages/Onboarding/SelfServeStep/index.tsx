6c23d234a8c817c8cf92fb207adba183ad4456b2
anastasiia <anastasiya1155@gmail.com>
2023-05-11 14:14:58 +0200

    add back self serve onboarding

---
+++ client/src/pages/Onboarding/SelfServeStep/index.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import DialogText from '../DialogText';
+import Button from '../../../components/Button';
+import { GitHubLogo } from '../../../icons';
+import { githubWebLogin } from '../../../services/api';
+
+const SelfServeStep = () => {
+  const [loginUrl, setLoginUrl] = useState('');
+
+  useEffect(() => {
+    githubWebLogin().then((resp) => {
+      setLoginUrl(resp.oauth_url);
+    });
+  }, []);
+
+  return (
+    <>
+      <DialogText
+        title="Sign In"
+        description="Use GitHub to sign in to your account"
+      />
+      <a href={loginUrl} className="w-full flex flex-col">
+        <Button variant="primary" disabled={!loginUrl}>
+          <GitHubLogo /> Sign in with GitHub
+        </Button>
+      </a>
+    </>
+  );
+};
+
+export default SelfServeStep;
--- client/src/pages/Onboarding/index.tsx
+++ client/src/pages/Onboarding/index.tsx
@@ -18,9 +18,11 @@ import {
   saveUserData,
 } from '../../services/api';
 import SeparateOnboardingStep from '../../components/SeparateOnboardingStep';
+import StatusBar from '../../components/StatusBar';
 import GitHubConnect from './GitHubConnect';
 import UserForm from './UserForm';
 import FeaturesStep from './FeaturesStep';
+import SelfServeStep from './SelfServeStep';
 
 let onboardingFinished = false;
 
@@ -125,47 +127,67 @@ const Onboarding = () => {
   }, [form, envConfig.tracking_id]);
 
   return shouldShowWelcome ? (
-    <div className="fixed top-0 bottom-0 left-0 right-0 z-100 bg-bg-sub select-none">
-      {os.type === 'Darwin' && <NavBar isSkeleton />}
-      <img
-        src="/light.png"
-        alt=""
-        className="fixed -top-68 lg:-top-80 xl:-top-96 w-[90vw] lg:w-[80vw] xl:w-[69vw] right-0 pointer-events-none opacity-[0.16] z-50"
-      />
-      <div className="flex h-full justify-center mt-8">
-        <div className="w-full lg:w-1/2 h-full flex justify-center">
-          <div
-            className={`w-[512px] h-full flex flex-col items-center justify-center px-13 ${
-              isGitHubScreen ? 'gap-8' : 'gap-6'
-            }`}
-          >
-            {!isGitHubScreen ? (
-              <UserForm
-                form={form}
-                setForm={setForm}
-                setGitHubScreen={setGitHubScreen}
-                onContinue={onSubmit}
-              />
-            ) : (
-              <GitHubConnect goBack={() => setGitHubScreen(false)} />
-            )}
+    !isSelfServe ? (
+      <div className="text-label-title">
+        <NavBar isSkeleton />
+        <div
+          className={`flex justify-center items-start mt-8 w-screen overflow-auto relative h-[calc(100vh-4rem)]`}
+        >
+          <div className="fixed top-0 bottom-0 left-0 right-0 mt-8 mb-16 bg-[url('/onboarding-background.png')] bg-cover z-50">
+            <div className="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center overflow-auto bg-bg-base/75">
+              <div className="flex flex-col items-center max-w-md2 w-full">
+                <div className="bg-bg-base border border-bg-border rounded-lg shadow-high p-6 flex flex-col flex-1 gap-8 w-full max-w-md2 relative max-h-[calc(100vh-12rem)]">
+                  <SelfServeStep />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div
-          className={`w-1/2 h-full hidden lg:flex justify-center items-center border-l border-bg-border relative 
+        <StatusBar />
+      </div>
+    ) : (
+      <div className="fixed top-0 bottom-0 left-0 right-0 z-100 bg-bg-sub select-none">
+        {os.type === 'Darwin' && <NavBar isSkeleton />}
+        <img
+          src="/light.png"
+          alt=""
+          className="fixed -top-68 lg:-top-80 xl:-top-96 w-[90vw] lg:w-[80vw] xl:w-[69vw] right-0 pointer-events-none opacity-[0.16] z-50"
+        />
+        <div className="flex h-full justify-center mt-8">
+          <div className="w-full lg:w-1/2 h-full flex justify-center">
+            <div
+              className={`w-[512px] h-full flex flex-col items-center justify-center px-13 ${
+                isGitHubScreen ? 'gap-8' : 'gap-6'
+              }`}
+            >
+              {!isGitHubScreen ? (
+                <UserForm
+                  form={form}
+                  setForm={setForm}
+                  setGitHubScreen={setGitHubScreen}
+                  onContinue={onSubmit}
+                />
+              ) : (
+                <GitHubConnect goBack={() => setGitHubScreen(false)} />
+              )}
+            </div>
+          </div>
+          <div
+            className={`w-1/2 h-full hidden lg:flex justify-center items-center border-l border-bg-border relative 
         before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:bg-[url('/grainy-pattern.png')] 
         before:bg-repeat before:mix-blend-soft-light before:opacity-[0.14]`}
-        >
-          <div className="w-[585px]">
-            <img
-              srcSet="/chatsImage-big.png 3x"
-              src="/chatsImage-small.png"
-              alt="chats in bloop"
-            />
+          >
+            <div className="w-[585px]">
+              <img
+                srcSet="/chatsImage-big.png 3x"
+                src="/chatsImage-small.png"
+                alt="chats in bloop"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   ) : (
     <SeparateOnboardingStep
       isVisible={shouldShowPopup}
