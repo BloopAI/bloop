8eb1b6cf9c26741aac66d66829b98587be85a269
Akshay <nerdy@peppe.rs>
2023-05-26 20:55:46 +0530

    fix chunker bugs (#565)
    * fix chunker bugs
    
    - bug that caused chunks to cut off midway
    - bug that caused chunks to start with a character followed by a newline
    
    * add tests
    
    * add long lines test

--- server/bleep/src/semantic/chunk.rs
+++ server/bleep/src/semantic/chunk.rs
@@ -165,41 +165,26 @@ fn add_token_range<'s>(
     o: Range<usize>,
     last_line: &mut usize,
     last_byte: &mut usize,
-    go_back_to_line_start: bool,
 ) {
     let start_byte = offsets[o.start].0;
-    let line_start_byte = if go_back_to_line_start {
-        src[..start_byte]
-            .char_indices()
-            .rev()
-            .find_map(|(i, c)| (c == '\n').then_some(i + 1))
-            .unwrap_or(0)
-    } else {
-        start_byte
-    };
     let end_byte = offsets.get(o.end).map_or(src.len(), |&(s, _)| s);
-    let Some(trimmed_end_byte) = src[..end_byte]
-        .char_indices()
-        .rev()
-        .find_map(|(i, c)| (!c.is_whitespace()).then_some(i)) else { return };
-    if trimmed_end_byte <= start_byte {
+
+    if end_byte <= start_byte {
         return;
     }
+
     debug_assert!(
         o.end - o.start < 256,
         "chunk too large: {} tokens in {:?} bytes {:?}",
         o.end - o.start,
         o,
-        line_start_byte..trimmed_end_byte
+        start_byte..end_byte
     );
-    let start = point(src, line_start_byte, *last_line, *last_byte);
-    let end = point(src, trimmed_end_byte, *last_line, *last_byte);
+
+    let start = point(src, start_byte, *last_line, *last_byte);
+    let end = point(src, end_byte, *last_line, *last_byte);
     (*last_line, *last_byte) = (start.line, start.byte);
-    chunks.push(Chunk::new(
-        &src[line_start_byte..trimmed_end_byte],
-        start,
-        end,
-    ));
+    chunks.push(Chunk::new(&src[start_byte..end_byte], start, end));
 }
 
 /// This tries to split the code by lines and add as much tokens as possible until reaching
@@ -287,10 +272,9 @@ pub fn by_tokens<'s>(
                 &mut chunks,
                 src,
                 offsets,
-                start..end_limit,
+                start..end_limit + 1,
                 &mut last_line,
                 &mut last_byte,
-                false,
             );
         }
         if end_limit == offsets_len {
@@ -302,7 +286,8 @@ pub fn by_tokens<'s>(
         let next_newline_diff =
             (mid..end_limit).find(|&i| src[offsets[i].0..offsets[i + 1].0].contains('\n'));
         let prev_newline_diff = (start + (diff / 2)..mid)
-            .rfind(|&i| src[offsets[i].0..offsets[i + 1].0].contains('\n'));
+            .rfind(|&i| src[offsets[i].0..offsets[i + 1].0].contains('\n'))
+            .map(|t| t + 1);
         start = match (next_newline_diff, prev_newline_diff) {
             (Some(n), None) | (None, Some(n)) => n,
             (Some(n), Some(p)) => {
@@ -361,15 +346,20 @@ mod tests {
     use super::*;
     use std::env;
 
-    #[test]
-    pub fn test_empty() {
+    fn minilm() -> Tokenizer {
         let cur_dir = env::current_dir().unwrap();
         let base_dir = cur_dir.ancestors().nth(2).unwrap();
         let tok_json = base_dir.join("model/tokenizer.json");
         let tokenizer = tokenizers::Tokenizer::from_file(tok_json).unwrap();
+        tokenizer
+    }
+
+    #[test]
+    pub fn empty() {
+        let tokenizer = minilm();
         let token_bounds = 50..256;
         let max_lines = 15;
-        let no_tokens = by_tokens(
+        let no_tokens = super::by_tokens(
             "bloop",
             "rmpty.rs",
             "",
@@ -382,13 +372,12 @@ mod tests {
     }
 
     #[test]
-    pub fn test_by_tokens() {
-        let cur_dir = env::current_dir().unwrap();
-        let base_dir = cur_dir.ancestors().nth(2).unwrap();
-        let tok_json = base_dir.join("model/tokenizer.json");
-        let tokenizer = tokenizers::Tokenizer::from_file(tok_json).unwrap();
+    pub fn by_tokens() {
+        let tokenizer = minilm();
         let token_bounds = 50..256;
         let max_lines = 15;
+        let cur_dir = env::current_dir().unwrap();
+        let base_dir = cur_dir.ancestors().nth(2).unwrap();
         let walk = ignore::WalkBuilder::new(base_dir)
             .standard_filters(true)
             .build();
@@ -400,7 +389,7 @@ mod tests {
                 continue;
             }
             let Ok(src) = std::fs::read_to_string(file.path()) else { continue };
-            let chunks = by_tokens(
+            let chunks = super::by_tokens(
                 "bloop",
                 &file.path().to_string_lossy(),
                 &src,
@@ -422,4 +411,416 @@ mod tests {
             "Average chunk size should be more than {min_avg_size}, was {avg_size}",
         );
     }
+
+    #[test]
+    pub fn chunks_within_token_limit() {
+        let tokenizer = minilm();
+        let max_lines = 15;
+
+        let chunks = super::by_tokens(
+            "bloop",
+            "src/config.rs",
+            SRC,
+            &tokenizer,
+            50..256,
+            max_lines,
+            OverlapStrategy::Partial(0.5),
+        );
+
+        for chunk in chunks {
+            let len = tokenizer.encode(chunk.data, false).unwrap().len();
+            assert!(
+                len.saturating_sub(256) < 10,
+                "chunk length ({len}) was not less than 256\n\n{}\n",
+                chunk.data
+            )
+        }
+    }
+
+    #[test]
+    pub fn chunks_over_long_lined_file() {
+        let tokenizer = minilm();
+        let max_lines = 15;
+
+        // squish SRC into one big single-lined string
+        let src = SRC.lines().collect::<String>();
+
+        let chunks = super::by_tokens(
+            "bloop",
+            "src/config.rs",
+            &src,
+            &tokenizer,
+            50..256,
+            max_lines,
+            OverlapStrategy::Partial(0.5),
+        );
+
+        for chunk in chunks {
+            let len = tokenizer.encode(chunk.data, false).unwrap().len();
+            assert!(
+                len.saturating_sub(256) < 10,
+                "chunk length ({len}) was not less than 256\n\n{}\n",
+                chunk.data
+            )
+        }
+    }
+
+    static SRC: &str = r#"
+use crate::{semantic::chunk::OverlapStrategy, state::StateSource};
+use anyhow::{Context, Result};
+use clap::Parser;
+
+use secrecy::{ExposeSecret, SecretString};
+use serde::{Deserialize, Serialize, Serializer};
+use std::path::{Path, PathBuf};
+
+#[derive(Serialize, Deserialize, Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Configuration {
+    //
+    // Core configuration options
+    //
+    #[clap(short, long)]
+    #[serde(skip)]
+    /// If a config file is given, it will override _all_ command line parameters!
+    pub config_file: Option<PathBuf>,
+
+    #[clap(flatten)]
+    #[serde(default)]
+    pub source: StateSource,
+
+    #[clap(short, long, default_value_os_t = default_data_dir())]
+    #[serde(default = "default_data_dir")]
+    /// Directory to store indexes
+    pub index_dir: PathBuf,
+
+    /// Directory to store persistent data
+    #[clap(long, default_value_os_t = default_data_dir())]
+    #[serde(default = "default_data_dir")]
+    pub data_dir: PathBuf,
+
+    #[clap(long, default_value_t = false)]
+    #[serde(skip)]
+    /// Quit after indexing the specified repos
+    pub index_only: bool,
+
+    #[clap(long, default_value_t = false)]
+    #[serde(default)]
+    /// Disable periodic reindexing, and `git pull` on remote repositories.
+    pub disable_background: bool,
+
+    #[clap(long, default_value_t = false)]
+    #[serde(default)]
+    /// Disable system-native notification backends to detect new git commits immediately.
+    pub disable_fsevents: bool,
+
+    #[clap(short, long, default_value_t = default_buffer_size())]
+    #[serde(default = "default_buffer_size")]
+    /// Size of memory to use for file indexes
+    pub buffer_size: usize,
+
+    #[clap(short, long, default_value_t = default_repo_buffer_size())]
+    #[serde(default = "default_repo_buffer_size")]
+    /// Size of memory to use for repo indexes
+    pub repo_buffer_size: usize,
+
+    #[clap(short, long, default_value_t = default_parallelism())]
+    #[serde(default = "default_parallelism")]
+    /// Maximum number of parallel background threads
+    pub max_threads: usize,
+
+    #[clap(long, default_value_t = default_host())]
+    #[serde(default = "default_host")]
+    /// Bind the webserver to `<port>`
+    pub host: String,
+
+    #[clap(long, default_value_t = default_port())]
+    #[serde(default = "default_port")]
+    /// Bind the webserver to `<host>`
+    pub port: u16,
+
+    //
+    // External dependencies
+    //
+    #[clap(long, default_value_t = default_answer_api_url())]
+    #[serde(default = "default_answer_api_url")]
+    /// URL for the answer-api
+    pub answer_api_url: String,
+
+    #[clap(long)]
+    /// Key for analytics backend
+    pub analytics_key: Option<String>,
+
+    #[clap(long)]
+    /// Key for analytics backend for frontend
+    pub analytics_key_fe: Option<String>,
+
+    #[clap(long)]
+    /// Analytics data plane identifier
+    pub analytics_data_plane: Option<String>,
+
+    #[clap(long)]
+    /// Sentry Data Source Name
+    pub sentry_dsn: Option<String>,
+
+    #[clap(long)]
+    /// Sentry Data Source Name for frontend
+    pub sentry_dsn_fe: Option<String>,
+
+    //
+    // Semantic values
+    //
+    #[clap(long)]
+    /// URL for the qdrant server
+    pub qdrant_url: Option<String>,
+
+    #[clap(long, default_value_os_t = default_model_dir())]
+    #[serde(default = "default_model_dir")]
+    /// Path to the embedding model directory
+    pub model_dir: PathBuf,
+
+    #[clap(long, default_value_t = default_max_chunk_tokens())]
+    #[serde(default = "default_max_chunk_tokens")]
+    /// Maximum number of tokens in a chunk (should be the model's input size)
+    pub max_chunk_tokens: usize,
+
+    #[clap(long)]
+    /// Chunking strategy
+    pub overlap: Option<OverlapStrategy>,
+
+    //
+    // Installation-specific values
+    //
+    #[clap(long)]
+    #[serde(serialize_with = "serialize_secret_opt_str", default)]
+    /// Github Client ID for either OAuth or GitHub Apps
+    pub github_client_id: Option<SecretString>,
+
+    // Github client secret
+    #[clap(long)]
+    #[serde(serialize_with = "serialize_secret_opt_str", default)]
+    pub github_client_secret: Option<SecretString>,
+
+    #[clap(long)]
+    /// GitHub App ID
+    pub github_app_id: Option<u64>,
+
+    #[clap(long)]
+    /// GitHub App installation ID
+    pub github_app_install_id: Option<u64>,
+
+    #[clap(long)]
+    /// Path to a GitHub private key file, for signing access token requests
+    pub github_app_private_key: Option<PathBuf>,
+
+    #[clap(long)]
+    #[serde(serialize_with = "serialize_secret_opt_str", default)]
+    /// Bot secret token
+    pub bot_secret: Option<SecretString>,
+
+    //
+    // Cloud deployment values
+    //
+    #[clap(long)]
+    /// Full instance domain, e.g. `foo.bloop.ai`
+    pub instance_domain: Option<String>,
+
+    /// Path to built front-end folder
+    #[clap(long)]
+    pub frontend_dist: Option<PathBuf>,
+}
+
+macro_rules! right_if_default {
+    ($left:expr, $right:expr, $default:expr) => {
+        if $left == $default {
+            $right
+        } else {
+            $left
+        }
+    };
+}
+
+impl Configuration {
+    pub fn read(file: impl AsRef<Path>) -> Result<Self> {
+        let file = std::fs::File::open(file)?;
+        Ok(serde_json::from_reader::<_, Self>(file)?)
+    }
+
+    pub fn from_cli() -> Result<Self> {
+        Ok(Self::try_parse()?)
+    }
+
+    pub fn index_path(&self, name: impl AsRef<Path>) -> impl AsRef<Path> {
+        self.index_dir.join(name)
+    }
+
+    pub fn github_client_id_and_secret(&self) -> Option<(&str, &str)> {
+        let id = self.github_client_id.as_ref()?.expose_secret();
+        let secret = self.github_client_secret.as_ref()?.expose_secret();
+        Some((id, secret))
+    }
+
+    pub fn cli_overriding_config_file() -> Result<Self> {
+        let cli = Self::from_cli()?;
+        let Ok(file) = cli
+	    .config_file
+	    .as_ref()
+	    .context("no config file specified")
+	    .and_then(Self::read) else
+	{
+	    return Ok(cli);
+	};
+
+        Ok(Self::merge(file, cli))
+    }
+
+    /// Merge 2 configurations with values from `b` taking precedence
+    ///
+    /// In case a default value is recognized in *either* sides,
+    /// always the non-default value will be used for the resulting
+    /// configuration.
+    pub fn merge(a: Self, b: Self) -> Self {
+        // the values here are in the order they're listed in the
+        // original `Configuration` declaration
+        Self {
+            config_file: b.config_file.or(a.config_file),
+
+            source: right_if_default!(b.source, a.source, Default::default()),
+
+            index_dir: right_if_default!(b.index_dir, a.index_dir, default_data_dir()),
+
+            data_dir: b.data_dir,
+
+            index_only: b.index_only | a.index_only,
+
+            disable_background: b.disable_background | a.disable_background,
+
+            disable_fsevents: b.disable_fsevents | a.disable_fsevents,
+
+            buffer_size: right_if_default!(b.buffer_size, a.buffer_size, default_buffer_size()),
+
+            repo_buffer_size: right_if_default!(
+                b.repo_buffer_size,
+                a.repo_buffer_size,
+                default_repo_buffer_size()
+            ),
+
+            max_threads: right_if_default!(b.max_threads, a.max_threads, default_parallelism()),
+
+            host: right_if_default!(b.host, a.host, default_host()),
+
+            port: right_if_default!(b.port, a.port, default_port()),
+
+            model_dir: right_if_default!(b.model_dir, a.model_dir, default_model_dir()),
+
+            max_chunk_tokens: right_if_default!(
+                b.max_chunk_tokens,
+                a.max_chunk_tokens,
+                default_max_chunk_tokens()
+            ),
+
+            overlap: b.overlap.or(a.overlap),
+
+            frontend_dist: b.frontend_dist.or(a.frontend_dist),
+
+            qdrant_url: b.qdrant_url.or(a.qdrant_url),
+
+            answer_api_url: right_if_default!(
+                b.answer_api_url,
+                a.answer_api_url,
+                default_answer_api_url()
+            ),
+
+            github_client_id: b.github_client_id.or(a.github_client_id),
+
+            github_client_secret: b.github_client_secret.or(a.github_client_secret),
+
+            github_app_id: b.github_app_id.or(a.github_app_id),
+
+            github_app_install_id: b.github_app_install_id.or(a.github_app_install_id),
+
+            github_app_private_key: b.github_app_private_key.or(a.github_app_private_key),
+
+            instance_domain: b.instance_domain.or(a.instance_domain),
+
+            bot_secret: b.bot_secret.or(a.bot_secret),
+
+            analytics_key: b.analytics_key.or(a.analytics_key),
+            analytics_key_fe: b.analytics_key_fe.or(a.analytics_key_fe),
+
+            analytics_data_plane: b.analytics_data_plane.or(a.analytics_data_plane),
+
+            sentry_dsn: b.sentry_dsn.or(a.sentry_dsn),
+
+            sentry_dsn_fe: b.sentry_dsn_fe.or(a.sentry_dsn_fe),
+        }
+    }
+}
+
+pub fn serialize_secret_opt_str<S>(
+    opt_secstr: &Option<SecretString>,
+    ser: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    match opt_secstr {
+        Some(secstr) => ser.serialize_some(secstr.expose_secret()),
+        None => ser.serialize_none(),
+    }
+}
+
+pub fn serialize_secret_str<S>(secstr: &SecretString, ser: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    ser.serialize_str(secstr.expose_secret())
+}
+
+//
+// Configuration defaults
+//
+fn default_data_dir() -> PathBuf {
+    match directories::ProjectDirs::from("ai", "bloop", "bleep") {
+        Some(dirs) => dirs.data_dir().to_owned(),
+        None => "bloop_index".into(),
+    }
+}
+
+fn default_model_dir() -> PathBuf {
+    "model".into()
+}
+
+pub fn default_parallelism() -> usize {
+    std::thread::available_parallelism().unwrap().get()
+}
+
+pub const fn minimum_parallelism() -> usize {
+    1
+}
+
+const fn default_buffer_size() -> usize {
+    100_000_000
+}
+
+const fn default_repo_buffer_size() -> usize {
+    30_000_000
+}
+
+const fn default_port() -> u16 {
+    7878
+}
+
+fn default_host() -> String {
+    String::from("127.0.0.1")
+}
+
+fn default_answer_api_url() -> String {
+    String::from("http://127.0.0.1:7879")
+}
+
+fn default_max_chunk_tokens() -> usize {
+    256
+}
+    "#;
 }
