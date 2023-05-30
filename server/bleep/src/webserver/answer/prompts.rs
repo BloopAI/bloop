33baf60238552f0c0b76a8f3e1eb47ee0e39a429
Gabriel Gordon-Hall <ggordonhall@gmail.com>
2023-05-30 19:09:43 +0100

    fmt

--- server/bleep/src/webserver/answer/prompts.rs
+++ server/bleep/src/webserver/answer/prompts.rs
@@ -23,25 +23,25 @@ impl fmt::Display for Tool {
 
 pub fn system(paths: &Vec<String>) -> String {
     let tools = [
-      Tool { 
+      Tool {
         name: "codeSearch", 
         description: "Search the contents of files in a codebase semantically. Results will not necessarily match search terms exactly, but should be related.", 
         schema: "{\"name\": \"code\", \"args\": [SEARCH_TERMS // str]}", 
         examples: "[[\"code\", \"backend error types\"], [\"code\", \"react functional components\"]]"
           },
-      Tool { 
+      Tool {
         name: "pathSearch", 
         description: "Search the pathnames in a codebase. Results may not be exact matches, but will be similar by some edit-distance. Use when you want to find a specific file or directory.", 
         schema: "{\"name\": \"path\", \"args\": [SEARCH_TERMS // str]}", 
         examples: "[[\"path\", \"server/src\"], [\"path\", \".tsx\"], [\"path\", \"examples/android\"]]"
       },
-      Tool { 
+      Tool {
         name: "processFiles", 
         description: "Read one or more files and extract the line ranges which are relevant to the search terms.", 
         schema: "{\"name\": \"proc\", \"args\": [SEARCH_TERMS // str, ARRAY_OF_PATH_ALIASES // int[]}", 
         examples: "[[\"proc\", \"find all the functional react components\", [2, 5]], [\"proc\", \"where are error types\", [0]], [\"proc\", \"gitoxide initialisation\", [2, 5, 8]]]"
     },
-      Tool { 
+      Tool {
         name: "none", 
         description: "You have enough information to answer the user's query. This is the final step, and signals that you have enough information to respond to the user's query. ARRAY_OF_PATH_ALIASES contains the aliases of the paths which are particularly relevant to the query.", 
         schema: "{\"name\": \"none\", \"args\": [ARRAY_OF_PATH_ALIASES // int[]]}", 
