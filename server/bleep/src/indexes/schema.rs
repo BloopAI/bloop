eccab85c213eaba065f9b1682f6145adef6a164c
rsdy <p@symmetree.dev>
2023-06-06 17:36:22 +0200

    Add documentation

--- server/bleep/src/indexes/schema.rs
+++ server/bleep/src/indexes/schema.rs
@@ -10,6 +10,8 @@ use {
     std::{sync::RwLock, time::Instant},
 };
 
+/// A schema for indexing all files and directories, linked to a
+/// single repository on disk.
 #[derive(Clone)]
 pub struct File {
     pub(super) config: Arc<Configuration>,
@@ -19,62 +21,67 @@ pub struct File {
     #[cfg(feature = "debug")]
     histogram: Arc<RwLock<Histogram>>,
 
-    // Path to the indexed file or directory on disk
+    /// Path to the indexed file or directory on disk
     pub entry_disk_path: Field,
-    // Path to the root of the repo on disk
+    /// Path to the root of the repo on disk
     pub repo_disk_path: Field,
-    // Path to the file, relative to the repo root
+    /// Path to the file, relative to the repo root
     pub relative_path: Field,
 
-    // Unique repo identifier, of the form:
-    //  local: local//path/to/repo
-    // github: github.com/org/repo
+    /// Unique repo identifier, of the form:
+    ///  local: local//path/to/repo
+    /// github: github.com/org/repo
     pub repo_ref: Field,
 
-    // Indexed repo name, of the form:
-    //  local: repo
-    // github: github.com/org/repo
+    /// Indexed repo name, of the form:
+    ///  local: repo
+    /// github: github.com/org/repo
     pub repo_name: Field,
 
     pub content: Field,
     pub line_end_indices: Field,
 
-    // a flat list of every symbol's text, for searching, e.g.: ["File", "Repo", "worker"]
+    /// a flat list of every symbol's text, for searching, e.g.:
+    /// ["File", "Repo", "worker"]
     pub symbols: Field,
     pub symbol_locations: Field,
 
-    // fast fields for scoring
+    /// fast fields for scoring
     pub lang: Field,
     pub avg_line_length: Field,
     pub last_commit_unix_seconds: Field,
 
-    // fast byte versions of certain fields for collector-level filtering
+    /// fast byte versions of certain fields for collector-level filtering
     pub raw_content: Field,
     pub raw_repo_name: Field,
     pub raw_relative_path: Field,
 
-    // list of branches in which this file can be found
+    /// list of branches in which this file can be found
     pub branches: Field,
 
-    // Whether this entry is a file or a directory
+    /// Whether this entry is a file or a directory
     pub is_directory: Field,
 }
 
+/// An index representing a repository to allow free-text search on
+/// repository names
 pub struct Repo {
     pub(super) schema: Schema,
-    // Path to the root of the repo on disk
+
+    /// Path to the root of the repo on disk
     pub disk_path: Field,
-    // Name of the org
+
+    /// Name of the org
     pub org: Field,
 
-    // Indexed repo name, of the form:
-    //  local: repo
-    // github: github.com/org/repo
+    /// Indexed repo name, of the form:
+    ///  local: repo
+    /// github: github.com/org/repo
     pub name: Field,
     pub raw_name: Field,
 
-    // Unique repo identifier, of the form:
-    //  local: local//path/to/repo
-    // github: github.com/org/repo
+    /// Unique repo identifier, of the form:
+    ///  local: local//path/to/repo
+    /// github: github.com/org/repo
     pub repo_ref: Field,
 }
