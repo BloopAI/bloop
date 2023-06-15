aa113cbdc7fb775c2f40e204cd6c2e3fe36ab02d
rsdy <p@symmetree.dev>
2023-06-15 16:51:45 +0200

    Make misc endpoints recognize branch filters

--- server/bleep/src/webserver/answer.rs
+++ server/bleep/src/webserver/answer.rs
@@ -141,6 +141,18 @@ pub(super) async fn _handle(
 
     let q = params.q;
 
+    ctx.branch = 'branch: {
+        let Ok(parsed) = parser::parse_nl(&q)
+	else {
+	    break 'branch None;
+	};
+
+        parsed
+            .as_semantic()
+            .and_then(|s| s.branch.iter().next())
+            .map(|l| l.regex_str().to_string())
+    };
+
     let stream = async_stream::try_stream! {
         let mut action = Action::Query(q);
 
@@ -583,7 +595,7 @@ impl Conversation {
                     .app
                     .indexes
                     .file
-                    .by_path(repo_ref, &path)
+                    .by_path(repo_ref, &path, ctx.branch.as_deref())
                     .await
                     .with_context(|| format!("failed to read path: {path}"))?
                     .content
@@ -821,7 +833,7 @@ impl Conversation {
                     .app
                     .indexes
                     .file
-                    .by_path(&self.repo_ref, &path)
+                    .by_path(&self.repo_ref, &path, ctx.branch.as_deref())
                     .await
                     .with_context(|| format!("failed to read path: {}", path))?
                     .content;
@@ -1098,7 +1110,7 @@ impl Conversation {
                     .app
                     .indexes
                     .file
-                    .by_path(repo_ref, &path)
+                    .by_path(repo_ref, &path, ctx.branch.as_deref())
                     .await
                     .unwrap()
                     .content;
@@ -1303,6 +1315,7 @@ struct AppContext {
     query_id: uuid::Uuid,
     thread_id: uuid::Uuid,
     repo_ref: Option<RepoRef>,
+    branch: Option<String>,
 
     /// Indicate whether the request was answered.
     ///
@@ -1324,6 +1337,7 @@ impl AppContext {
             thread_id: uuid::Uuid::nil(),
             repo_ref: None,
             req_complete: false,
+            branch: None,
         })
     }
 
