cb75155accf9772112ed59de827ed62e31bb1855
anastasiia <anastasiya1155@gmail.com>
2023-06-28 11:50:55 +0300

    fix scrolling

--- client/src/pages/ArticleResponse/index.tsx
+++ client/src/pages/ArticleResponse/index.tsx
@@ -26,59 +26,65 @@ const ArticleResponse = ({ recordId, threadId }: Props) => {
   );
 
   return (
-    <div className="p-8 flex-1 overflow-x-auto mx-auto max-w-3xl box-content article-response body-m text-label-base pb-44">
-      <ReactMarkdown
-        components={{
-          a(props) {
-            const [filePath, lines] = props.href?.split('#') || [];
-            const [start, end] = lines?.split('-').map((l) => l.slice(1)) || [];
-            return (
-              <FileChip
-                fileName={filePath || ''}
-                onClick={() =>
-                  openFileModal(filePath, start ? `${start}_${end}` : undefined)
-                }
-              />
-            );
-          },
-          code({ node, inline, className, children, ...props }) {
-            const matchLang = /language-(\w+)/.exec(className || '');
-            const matchPath = /path:(.+),/.exec(className || '');
-            const matchLines = /lines:(.+)/.exec(className || '');
-            return !inline &&
-              matchLang?.[1] &&
-              typeof children[0] === 'string' ? (
-              <NewCode
-                code={`${
-                  matchPath?.[1]
-                    ? `// ${matchPath[1]}${
-                        matchLines?.[1]
-                          ? `:${matchLines?.[1]
-                              .split('-')
-                              .map((l) => l.slice(1))
-                              .join(':')}
+    <div className="overflow-auto p-8 w-screen">
+      <div className="flex-1 mx-auto max-w-3xl box-content article-response body-m text-label-base pb-44">
+        <ReactMarkdown
+          components={{
+            a(props) {
+              const [filePath, lines] = props.href?.split('#') || [];
+              const [start, end] =
+                lines?.split('-').map((l) => l.slice(1)) || [];
+              return (
+                <FileChip
+                  fileName={filePath || ''}
+                  onClick={() =>
+                    openFileModal(
+                      filePath,
+                      start ? `${start}_${end}` : undefined,
+                    )
+                  }
+                />
+              );
+            },
+            code({ node, inline, className, children, ...props }) {
+              const matchLang = /language-(\w+)/.exec(className || '');
+              const matchPath = /path:(.+),/.exec(className || '');
+              const matchLines = /lines:(.+)/.exec(className || '');
+              return !inline &&
+                matchLang?.[1] &&
+                typeof children[0] === 'string' ? (
+                <NewCode
+                  code={`${
+                    matchPath?.[1]
+                      ? `// ${matchPath[1]}${
+                          matchLines?.[1]
+                            ? `:${matchLines?.[1]
+                                .split('-')
+                                .map((l) => l.slice(1))
+                                .join(':')}
 `
-                          : ''
-                      }`
-                    : ''
-                }${children[0].replace(/\n$/, '')}`}
-                language={matchLang[1]}
-              />
-            ) : (
-              <code {...props} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      >
-        {data?.isLoading
-          ? data?.results?.Article?.replace(
-              /\[\`[^`]*$|\[\`[^`]+\`\]\([^)]*$/,
-              '',
-            )
-          : data?.results?.Article}
-      </ReactMarkdown>
+                            : ''
+                        }`
+                      : ''
+                  }${children[0].replace(/\n$/, '')}`}
+                  language={matchLang[1]}
+                />
+              ) : (
+                <code {...props} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {data?.isLoading
+            ? data?.results?.Article?.replace(
+                /\[\`[^`]*$|\[\`[^`]+\`\]\([^)]*$/,
+                '',
+              )
+            : data?.results?.Article}
+        </ReactMarkdown>
+      </div>
     </div>
   );
 };
