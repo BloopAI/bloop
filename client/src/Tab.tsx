64bc4ac2061b3aa710a4e3f3c9a2e33053bba8a7
anastasiia <anastasiya1155@gmail.com>
2023-07-03 14:32:33 +0300

    remove redundant registration of routes

--- client/src/Tab.tsx
+++ client/src/Tab.tsx
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Settings from './components/Settings';
 import { UITabType } from './types/general';
 import { DeviceContextType } from './context/deviceContext';
@@ -15,8 +14,6 @@ import FileModalContainer from './pages/ResultModal/FileModalContainer';
 import { FileModalContextProvider } from './context/providers/FileModalContextProvider';
 import PromptGuidePopup from './components/PromptGuidePopup';
 import Onboarding from './pages/Onboarding';
-import PageTemplate from './components/PageTemplate';
-import HomePage from './pages/Home';
 
 type Props = {
   deviceContextValue: DeviceContextType;
@@ -38,12 +35,7 @@ class Tab extends PureComponent<Props> {
               <AppNavigationProvider tab={tab}>
                 <SearchContextProvider tab={tab}>
                   <ChatContextProvider>
-                    <Routes>
-                      <Route
-                        path="*"
-                        element={<ContentContainer tab={tab} />}
-                      />
-                    </Routes>
+                    <ContentContainer tab={tab} />
                     <Settings />
                     <ReportBugModal />
                     <FileModalContainer repoName={tab.repoName} />
