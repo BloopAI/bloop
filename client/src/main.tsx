5046f8d8be2a2ce02a5a6f7a83be30131ade9ebe
anastasiia <anastasiya1155@gmail.com>
2023-07-21 09:13:04 +0300

    add a polyfill for findLastIndex

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CloudApp from './CloudApp';
+import polyfills from './utils/polyfills';
+polyfills();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
