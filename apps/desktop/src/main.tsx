e00d807850edebb31d71fd1cdd183953e2253d10
Anastasiia Solop <35258279+anastasiya1155@users.noreply.github.com>
2023-07-26 16:10:27 +0300

    enable polyfill for findLastIndex in tauri (#784)

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import polyfills from '../../../client/src/utils/polyfills';
 import App from './App';
+polyfills();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
