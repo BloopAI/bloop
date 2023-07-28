7d24fe43ce05a4dc679cd733cf4740d37f3328c7
anastasiia <anastasiya1155@gmail.com>
2023-07-28 16:11:15 +0300

    update npm dependencies, trim unused packages

--- client/src/pages/ResultModal/index.tsx
+++ client/src/pages/ResultModal/index.tsx
@@ -4,13 +4,10 @@ import { CloseSign } from '../../icons';
 import Button from '../../components/Button';
 import CodeFull from '../../components/CodeBlock/CodeFull';
 import { FullResult } from '../../types/results';
-import CommitHistory from '../../components/CommitHistory';
-import { mockCommits, mockGitBlame } from '../../mocks';
 import { FullResultModeEnum } from '../../types/general';
 import ModalOrSidebar from '../../components/ModalOrSidebar';
 import ShareFileModal from '../../components/ShareFileModal';
 import { splitPathForBreadcrumbs } from '../../utils';
-import Contributors from '../../components/Contributors';
 import ModeToggle from './ModeToggle';
 import Subheader from './Subheader';
 
@@ -21,18 +18,8 @@ type Props = {
   setMode: (n: FullResultModeEnum) => void;
 };
 
-/*
-const tabs = [
-  { title: 'Code' },
-  { title: 'Blame' },
-  { title: 'Commits' },
-  { title: 'Authors' },
-];
-*/
-
 const ResultModal = ({ result, onResultClosed, mode, setMode }: Props) => {
   const { t } = useTranslation();
-  const [activeTab, setActiveTab] = useState(0);
   const [isShareOpen, setShareOpen] = useState(false);
 
   useEffect(() => {
@@ -62,62 +49,6 @@ const ResultModal = ({ result, onResultClosed, mode, setMode }: Props) => {
     [result?.hoverableRanges],
   );
 
-  const getContent = (result: FullResult) => {
-    switch (activeTab) {
-      case 0:
-      default:
-        return (
-          <div
-            className={`flex px-2 py-4 bg-bg-sub h-[calc(100vh-15rem)] overflow-y-auto p-3 pr-12`}
-          >
-            <CodeFull
-              code={result.code}
-              language={result.language}
-              relativePath={result.relativePath}
-              repoPath={result.repoPath}
-              repoName={result.repoName}
-              metadata={metadata}
-              scrollElement={null}
-              containerWidth={window.innerWidth * 0.6}
-              containerHeight={window.innerHeight - 15 * 16 - 32}
-            />
-          </div>
-        );
-      case 1:
-        return (
-          <div
-            className={`flex bg-bg-sub h-[calc(100vh-15rem)] overflow-y-auto`}
-          >
-            <CodeFull
-              code={result.code}
-              language={result.language}
-              relativePath={result.relativePath}
-              repoPath={result.repoPath}
-              repoName={result.repoName}
-              metadata={{
-                lexicalBlocks: [],
-                hoverableRanges: [],
-                blame: mockGitBlame,
-              }}
-              scrollElement={null}
-              containerWidth={window.innerWidth * 0.6}
-              containerHeight={window.innerHeight - 15 * 16}
-            />
-          </div>
-        );
-      case 2:
-        return (
-          <div
-            className={`flex bg-bg-sub h-[calc(100vh-15rem)] overflow-y-auto pl-3 pr-12`}
-          >
-            <CommitHistory commits={mockCommits} showFirstSeparator={false} />
-          </div>
-        );
-      case 3:
-        return <Contributors />;
-    }
-  };
-
   return (
     <>
       <ModalOrSidebar
@@ -139,13 +70,6 @@ const ResultModal = ({ result, onResultClosed, mode, setMode }: Props) => {
             />
           )}
           <div className="flex gap-2">
-            {/*<SelectToggleButton onlyIcon title="Star">*/}
-            {/*  <Star />*/}
-            {/*</SelectToggleButton>*/}
-            {/*<Button variant="primary" onClick={() => setShareOpen(true)}>*/}
-            {/*  Share*/}
-            {/*  <ArrowBoxOut />*/}
-            {/*</Button>*/}
             <Button
               onlyIcon
               variant="tertiary"
