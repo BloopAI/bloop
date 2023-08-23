5da9d77b74bfcb490265f31592d191f5533ac3ff
anastasiia <anastasiya1155@gmail.com>
2023-08-23 16:45:34 +0300

    remember repo choice

--- client/src/pages/StudioTab/Content.tsx
+++ client/src/pages/StudioTab/Content.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import * as Sentry from '@sentry/react';
 import { Trans, useTranslation } from 'react-i18next';
 import ErrorFallback from '../../components/ErrorFallback';
@@ -6,12 +6,17 @@ import PageTemplate from '../../components/PageTemplate';
 import Button from '../../components/Button';
 import CodeStudioToken from '../../icons/CodeStudioToken';
 import { Template } from '../../icons';
-import { StudioLeftPanelType, StudioPanelDataType } from '../../types/general';
+import {
+  RepoType,
+  StudioLeftPanelType,
+  StudioPanelDataType,
+} from '../../types/general';
 import Conversation from './Conversation';
 import ContextPanel from './ContextPanel';
 import HistoryPanel from './HistoryPanel';
 import TemplatesPanel from './TemplatesPanel';
 import FilePanel from './FilePanel';
+import AddContextModal from './AddContextModal';
 
 const ContentContainer = () => {
   const { t } = useTranslation();
@@ -19,13 +24,28 @@ const ContentContainer = () => {
     type: StudioLeftPanelType.CONTEXT,
     data: null,
   });
+  const [isAddContextOpen, setAddContextOpen] = useState(false);
+
+  const handleAddContextClose = useCallback(() => setAddContextOpen(false), []);
+  const onNewContextSubmit = useCallback(
+    (repo: RepoType, branch: string, filePath: string) => {
+      setLeftPanel({
+        type: StudioLeftPanelType.FILE,
+        data: { repo, branch, filePath },
+      });
+    },
+    [],
+  );
 
   return (
     <PageTemplate renderPage="studio">
       <div className="flex flex-1 w-screen">
         <div className="w-1/2 border-r border-bg-border flex-shrink-0 flex-grow-0 flex flex-col">
           {leftPanel.type === StudioLeftPanelType.CONTEXT ? (
-            <ContextPanel setLeftPanel={setLeftPanel} />
+            <ContextPanel
+              setLeftPanel={setLeftPanel}
+              setAddContextOpen={setAddContextOpen}
+            />
           ) : leftPanel.type === StudioLeftPanelType.HISTORY ? (
             <HistoryPanel setLeftPanel={setLeftPanel} />
           ) : leftPanel.type === StudioLeftPanelType.TEMPLATES ? (
@@ -33,6 +53,11 @@ const ContentContainer = () => {
           ) : leftPanel.type === StudioLeftPanelType.FILE ? (
             <FilePanel setLeftPanel={setLeftPanel} {...leftPanel.data} />
           ) : null}
+          <AddContextModal
+            isVisible={isAddContextOpen}
+            onClose={handleAddContextClose}
+            onSubmit={onNewContextSubmit}
+          />
         </div>
         <div className="w-1/2 flex-shrink-0 flex-grow-0 flex flex-col">
           <div className="flex items-center justify-between gap-2 px-8 h-11.5 border-b border-bg-border bg-bg-sub shadow-low">
