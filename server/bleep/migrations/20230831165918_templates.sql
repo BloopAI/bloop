617cbdc911ba5fdb44974e511680481ffe303e1c
Louis Knight-Webb <louis@bloop.ai>
2023-08-31 20:50:37 +0100

    Add unit test default template and update sqlx offline build

--- server/bleep/migrations/20230831165918_templates.sql
+++ server/bleep/migrations/20230831165918_templates.sql
@@ -4,3 +4,13 @@ CREATE TABLE templates (
     modified_at DATETIME NOT NULL DEFAULT (datetime('now')),
     content TEXT NOT NULL
 );
+
+INSERT INTO templates (
+  id,
+  name,
+  content
+) VALUES (
+  'DEFAULT_UNIT_TEST',
+  'Unit Test',
+  'Perform the following steps:\n\nStep 1: Describe the purpose of the code\nStep 2: Identify which functions would benefit from unit testing\nStep 3: Write a unit test for each of those functions'
+);
