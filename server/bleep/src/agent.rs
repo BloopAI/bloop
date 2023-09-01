7ea88ec1ff5d08aee23f2f6e73c49fb01d3e7531
calyptobai <calyptobai@gmail.com>
2023-09-01 16:46:49 -0400

    Add canned conclusion when generation is cancelled

--- server/bleep/src/agent.rs
+++ server/bleep/src/agent.rs
@@ -80,6 +80,8 @@ impl Drop for Agent {
         match self.exchange_state {
             ExchangeState::Failed => {}
             ExchangeState::Pending => {
+                self.last_exchange_mut().apply_update(Update::Cancel);
+
                 self.track_query(
                     EventData::output_stage("cancelled")
                         .with_payload("message", "request was cancelled"),
