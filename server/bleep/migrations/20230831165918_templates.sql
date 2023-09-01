4bef3ed02d4c4d52f8736e632332eae39df3bc30
Louis Knight-Webb <louis@bloop.ai>
2023-09-01 09:42:30 +0100

    Fix newlines in default templates

--- server/bleep/migrations/20230831165918_templates.sql
+++ server/bleep/migrations/20230831165918_templates.sql
@@ -12,13 +12,25 @@ INSERT INTO templates (
 ) VALUES (
   'DEFAULT_UNIT_TEST',
   'Write a test',
-  'Perform the following steps:\n\nStep 1: Describe the purpose of the code\nStep 2: Identify which functions would benefit from unit testing\nStep 3: Write a unit test for each of those functions'
+  'Perform the following steps:
+
+Step 1: Describe the purpose of the code
+Step 2: Identify which functions would benefit from unit testing
+Step 3: Write a unit test for each of those functions'
 ), (
   'DEFAULT_CODE_REVIEW',
   'Code review',
-  'You''re a senior software engineer responsible for making sure no code makes it into production that doesn''t pass these rules:\n- All identifiers in the code have been named semantically so that their purpose is understood\n- All logic has been written in the most efficient way possible, optimising for the lowest compute consumption\n- Comments have been written where necessary to help future developers understand this code\n\nYour job is to 1) produce a list of issues related to the provided code and 2) provide example fixes for each issue'
+  'You''re a senior software engineer responsible for making sure no code makes it into production that doesn''t pass these rules:
+- All identifiers in the code have been named semantically so that their purpose is understood
+- All logic has been written in the most efficient way possible, optimising for the lowest compute consumption
+- Comments have been written where necessary to help future developers understand this code
+
+Your job is to 1) produce a list of issues related to the provided code and 2) provide example fixes for each issue'
 ), (
   'DEFAULT_EXPLANATION',
   'Explanation',
-  'I''m a new software engineer looking to learn about this codebase. As you''re a senior software engineer, could you please explain:\n1. The business purpose of this code\n2. The user journey\n3. How the code works, in the order of the user journey'
+  'I''m a new software engineer looking to learn about this codebase. As you''re a senior software engineer, could you please explain:
+1. The business purpose of this code
+2. The user journey
+3. How the code works, in the order of the user journey'
 );
