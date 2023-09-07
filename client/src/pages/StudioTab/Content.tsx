58eb8124bad5c38234a9e8d47931caedaec02c56
anastasiia <anastasiya1155@gmail.com>
2023-09-07 11:07:40 +0300

    move token limit exceeded message

--- client/src/pages/StudioTab/Content.tsx
+++ client/src/pages/StudioTab/Content.tsx
@@ -1,5 +1,6 @@
 import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
 import * as Sentry from '@sentry/react';
+import { Trans } from 'react-i18next';
 import ErrorFallback from '../../components/ErrorFallback';
 import PageTemplate from '../../components/PageTemplate';
 import {
@@ -12,6 +13,8 @@ import {
 import { getCodeStudio, patchCodeStudio } from '../../services/api';
 import { CodeStudioType, HistoryConversationTurn } from '../../types/api';
 import useResizeableSplitPanel from '../../hooks/useResizeableSplitPanel';
+import { TOKEN_LIMIT } from '../../consts/codeStudio';
+import { Info } from '../../icons';
 import ContextPanel from './ContextPanel';
 import HistoryPanel from './HistoryPanel';
 import TemplatesPanel from './TemplatesPanel';
@@ -199,76 +202,88 @@ const ContentContainer = ({ tab }: { tab: StudioTabType }) => {
 
   return (
     <PageTemplate renderPage="studio">
-      <div className="flex flex-1 w-screen">
-        {isHistoryOpen && (
-          <HistoryPanel
-            setIsHistoryOpen={setIsHistoryOpen}
-            studioId={tab.key}
-            refetchCodeStudio={refetchCodeStudio}
-            handlePreview={handlePreview}
-          />
+      <div className="w-screen flex flex-col overflow-auto">
+        {stateToShow.token_counts?.total > TOKEN_LIMIT && (
+          <div className="flex items-center gap-2 px-8 py-2 bg-bg-danger/12 select-none">
+            <Info raw sizeClassName="w-4.5 h-4.5" className="text-bg-danger" />
+            <p className="text-bg-danger caption">
+              <Trans>
+                Token limit exceeded. Reduce the number of context files or
+                messages to enable the ability to generate.
+              </Trans>
+            </p>
+          </div>
         )}
-        <div
-          className={`flex flex-1 relative ${
-            isHistoryOpen ? 'w-[calc(100%-13rem)]' : 'w-full'
-          }`}
-          ref={containerRef}
-        >
-          <div
-            className="w-1/2 flex-shrink-0 flex-grow-0 flex flex-col"
-            ref={leftPanelRef}
-          >
-            {leftPanel.type === StudioLeftPanelType.CONTEXT ? (
-              <ContextPanel
-                setLeftPanel={setLeftPanel}
-                setAddContextOpen={setAddContextOpen}
-                studioId={tab.key}
-                contextFiles={stateToShow.context}
-                tokensPerFile={stateToShow.token_counts?.per_file || []}
-                onFileRemove={onFileRemove}
-                onFileHide={onFileHide}
-                onFileAdded={onFileAdded}
-                tokensTotal={stateToShow.token_counts?.total}
-              />
-            ) : leftPanel.type === StudioLeftPanelType.TEMPLATES ? (
-              <TemplatesPanel setLeftPanel={setLeftPanel} />
-            ) : leftPanel.type === StudioLeftPanelType.FILE ? (
-              <FilePanel
-                {...leftPanel.data}
-                setLeftPanel={setLeftPanel}
-                onFileRangesChanged={onFileRangesChanged}
-                tokens={
-                  stateToShow.token_counts?.per_file[
-                    stateToShow.token_counts?.per_file.length - 1
-                  ]
-                }
-              />
-            ) : null}
-            <AddContextModal
-              isVisible={isAddContextOpen}
-              onClose={handleAddContextClose}
-              onSubmit={onFileSelected}
-              contextFiles={stateToShow.context}
-            />
-          </div>
-          <div
-            ref={dividerRef}
-            className="absolute top-0 left-1/2 transform -translate-x-1/2 w-2.5 h-full group cursor-col-resize flex-shrink-0"
-          >
-            <div className="mx-auto w-0.5 h-full bg-bg-border group-hover:bg-bg-main" />
-          </div>
-          <div
-            className="w-1/2 flex-shrink-0 flex-grow-0 flex flex-col"
-            ref={rightPanelRef}
-          >
-            <RightPanel
-              setLeftPanel={setLeftPanel}
-              studioId={tab.key}
-              messages={stateToShow.messages}
-              refetchCodeStudio={refetchCodeStudio}
-              tokensTotal={stateToShow.token_counts?.total}
+        <div className="flex flex-1 w-screen overflow-auto">
+          {isHistoryOpen && (
+            <HistoryPanel
               setIsHistoryOpen={setIsHistoryOpen}
+              studioId={tab.key}
+              refetchCodeStudio={refetchCodeStudio}
+              handlePreview={handlePreview}
             />
+          )}
+          <div
+            className={`flex flex-1 relative ${
+              isHistoryOpen ? 'w-[calc(100%-13rem)]' : 'w-full'
+            }`}
+            ref={containerRef}
+          >
+            <div
+              className="w-1/2 flex-shrink-0 flex-grow-0 flex flex-col"
+              ref={leftPanelRef}
+            >
+              {leftPanel.type === StudioLeftPanelType.CONTEXT ? (
+                <ContextPanel
+                  setLeftPanel={setLeftPanel}
+                  setAddContextOpen={setAddContextOpen}
+                  studioId={tab.key}
+                  contextFiles={stateToShow.context}
+                  tokensPerFile={stateToShow.token_counts?.per_file || []}
+                  onFileRemove={onFileRemove}
+                  onFileHide={onFileHide}
+                  onFileAdded={onFileAdded}
+                />
+              ) : leftPanel.type === StudioLeftPanelType.TEMPLATES ? (
+                <TemplatesPanel setLeftPanel={setLeftPanel} />
+              ) : leftPanel.type === StudioLeftPanelType.FILE ? (
+                <FilePanel
+                  {...leftPanel.data}
+                  setLeftPanel={setLeftPanel}
+                  onFileRangesChanged={onFileRangesChanged}
+                  tokens={
+                    stateToShow.token_counts?.per_file[
+                      stateToShow.token_counts?.per_file.length - 1
+                    ]
+                  }
+                />
+              ) : null}
+              <AddContextModal
+                isVisible={isAddContextOpen}
+                onClose={handleAddContextClose}
+                onSubmit={onFileSelected}
+                contextFiles={stateToShow.context}
+              />
+            </div>
+            <div
+              ref={dividerRef}
+              className="absolute top-0 left-1/2 transform -translate-x-1/2 w-2.5 h-full group cursor-col-resize flex-shrink-0"
+            >
+              <div className="mx-auto w-0.5 h-full bg-bg-border group-hover:bg-bg-main" />
+            </div>
+            <div
+              className="w-1/2 flex-shrink-0 flex-grow-0 flex flex-col"
+              ref={rightPanelRef}
+            >
+              <RightPanel
+                setLeftPanel={setLeftPanel}
+                studioId={tab.key}
+                messages={stateToShow.messages}
+                refetchCodeStudio={refetchCodeStudio}
+                tokensTotal={stateToShow.token_counts?.total}
+                setIsHistoryOpen={setIsHistoryOpen}
+              />
+            </div>
           </div>
         </div>
       </div>
