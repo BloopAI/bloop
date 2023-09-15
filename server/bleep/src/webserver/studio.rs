542d7079275776b8ba217331b58a37b367d53ffb
Gabriel Gordon-Hall <ggordonhall@gmail.com>
2023-09-15 12:06:53 +0100

    use gpt-3.5 for studio title generation

--- server/bleep/src/webserver/studio.rs
+++ server/bleep/src/webserver/studio.rs
@@ -715,7 +715,7 @@ async fn populate_studio_name(
     let llm_gateway = app
         .llm_gateway_client()
         .map_err(|e| Error::user(e).with_status(StatusCode::UNAUTHORIZED))?
-        .model(LLM_GATEWAY_MODEL)
+        .model("gpt-3.5-turbo-0613")
         .temperature(0.0);
 
     let messages = &[llm_gateway::api::Message::system(
