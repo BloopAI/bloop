84b8a4ba0454d25f24dd17de6bca3af2f6df7796
R Müller <22560219+rmuller-ml@users.noreply.github.com>
2023-09-15 04:57:25 -0400

    New "answer" prompt without "summary" and "title" (#937)
    * removing summary/title from prompt and using a single prompt for both scenarios
    
    * remove unused prompt
    
    ---------
    
    Co-authored-by: Gabriel Gordon-Hall <ggordonhall@gmail.com>

--- server/bleep/src/agent/prompts.rs
+++ server/bleep/src/agent/prompts.rs
@@ -149,32 +149,11 @@ A: "#
     )
 }
 
-/// Generate an answer article prompt.
-///
-/// The `multi` argument determines whether to use the "multiple file" variant of this prompt.
-pub fn answer_article_prompt(multi: bool, context: &str) -> String {
-    // Return different prompts depending on whether there is one or many aliases
-    let one_prompt = format!(
-        r#"{context}#####
+pub fn answer_article_prompt(context: &str) -> String {
+    format!(
+        r#"{context}####
 
-A user is looking at the code above, your job is to write an article answering their query.
-
-Your output will be interpreted as bloop-markdown which renders with the following rules:
-- Inline code must be expressed as a link to the correct line of code using the URL format: `[bar](src/foo.rs#L50)` or `[bar](src/foo.rs#L50-L54)`
-- Do NOT output bare symbols. ALL symbols must include a link
-  - E.g. Do not simply write `Bar`, write [`Bar`](src/bar.rs#L100-L105).
-  - E.g. Do not simply write "Foos are functions that create `Foo` values out of thin air." Instead, write: "Foos are functions that create [`Foo`](src/foo.rs#L80-L120) values out of thin air."
-- Only internal links to the current file work
-- Basic markdown text formatting rules are allowed, and you should use titles to improve readability
-
-Here is an example response:
-
-A function [`openCanOfBeans`](src/beans/open.py#L7-L19) is defined. This function is used to handle the opening of beans. It includes the variable [`openCanOfBeans`](src/beans/open.py#L9) which is used to store the value of the tin opener.
-"#
-    );
-
-    let many_prompt = format!(
-        r#"{context}Your job is to answer a query about a codebase using the information above.
+Your job is to answer a query about a codebase using the information above.
 
 Provide only as much information and code as is necessary to answer the query, but be concise. Keep number of quoted lines to a minimum when possible. If you do not have enough information needed to answer the query, do not make up an answer.
 When referring to code, you must provide an example in a code block.
@@ -192,12 +171,10 @@ Respect these rules at all times:
   - E.g. Do not simply write "Foos are functions that create `Foo` values out of thin air." Instead, write: "Foos are functions that create [`Foo`](src/foo.rs#L80-L120) values out of thin air."
 - Link all fields
   - E.g. Do not simply write: "It has one main field: `foo`." Instead, write: "It has one main field: [`foo`](src/foo.rs#L193)."
+- Do NOT link external urls not present in the context, do NOT link urls from the internet
 - Link all symbols, even when there are multiple in one sentence
   - E.g. Do not simply write: "Bars are [`Foo`]( that return a list filled with `Bar` variants." Instead, write: "Bars are functions that return a list filled with [`Bar`](src/bar.rs#L38-L57) variants."
-- Always begin your answer with an appropriate title
-- Always finish your answer with a summary in a [^summary] footnote
-  - If you do not have enough information needed to answer the query, do not make up an answer. Instead respond only with a [^summary] f
-ootnote that asks the user for more information, e.g. `assistant: [^summary]: I'm sorry, I couldn't find what you were looking for, could you provide more information?`
+  - If you do not have enough information needed to answer the query, do not make up an answer. Instead respond only with a footnote that asks the user for more information, e.g. `assistant: I'm sorry, I couldn't find what you were looking for, could you provide more information?`
 - Code blocks MUST be displayed to the user using XML in the following formats:
   - Do NOT output plain markdown blocks, the user CANNOT see them
   - To create new code, you MUST mimic the following structure (example given):
@@ -228,13 +205,7 @@ println!("hello world!");
   - Note: the line range is inclusive
 - When writing example code blocks, use `<GeneratedCode>`, and when quoting existing code, use `<QuotedCode>`.
 - You MUST use XML code blocks instead of markdown."#
-    );
-
-    if multi {
-        many_prompt
-    } else {
-        one_prompt
-    }
+    )
 }
 
 pub fn studio_article_prompt(context: &str) -> String {
