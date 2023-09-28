4e24077ce0a65d51721ce3a3249ec548e5413abf
anastasiia <anastasiya1155@gmail.com>
2023-09-28 13:21:52 +0300

    configure session replays on cloud

--- client/src/utils/services.ts
+++ client/src/utils/services.ts
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
 import { ExtraErrorData } from '@sentry/integrations';
 import { useEffect } from 'react';
 import {
@@ -8,34 +7,56 @@ import {
   useLocation,
   useNavigationType,
 } from 'react-router-dom';
+import { Integration } from '@sentry/types/types/integration';
 import { EnvConfig } from '../types/general';
 
+let isInitialized = false;
 export const initializeSentry = (envConfig: EnvConfig, release: string) => {
-  if (!envConfig.sentry_dsn_fe) {
+  if (!envConfig.sentry_dsn_fe || isInitialized) {
     return;
   }
-  Sentry.init({
-    dsn: envConfig.sentry_dsn_fe,
-    integrations: [
-      new BrowserTracing({
-        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
-          useEffect,
-          useLocation,
-          useNavigationType,
-          createRoutesFromChildren,
-          matchRoutes,
-        ),
+  const integrations: Integration[] = [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      ),
+    }),
+    new ExtraErrorData(),
+  ];
+  // if is on cloud
+  if (envConfig.org_name) {
+    integrations.push(
+      new Sentry.Replay({
+        maskAllText: false,
       }),
-      new ExtraErrorData(),
-    ],
-    environment: import.meta.env.MODE,
-    release,
-    tracesSampleRate: import.meta.env.MODE === 'development' ? 1.0 : 0.1,
-  });
+    );
+  }
+  try {
+    Sentry.init({
+      dsn: envConfig.sentry_dsn_fe,
+      integrations,
+      ...(envConfig.org_name
+        ? {
+            replaysSessionSampleRate: 1.0,
+            replaysOnErrorSampleRate: 1.0,
+          }
+        : {}),
+      environment: import.meta.env.MODE,
+      release,
+      tracesSampleRate: 0.1,
+    });
+    isInitialized = true;
 
-  Sentry.setUser({
-    id: envConfig.tracking_id,
-    username: envConfig.user_login,
-    device_id: envConfig.device_id,
-  });
+    Sentry.setUser({
+      id: envConfig.tracking_id,
+      username: envConfig.user_login,
+      device_id: envConfig.device_id,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
