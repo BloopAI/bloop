85746c5838e1830e5b38d5f91c7ecfa6b876e6f8
Akshay <nerdy@peppe.rs>
2023-10-03 12:33:01 +0530

    restore onnx-compatible tokenizer

--- server/bleep/src/lib.rs
+++ server/bleep/src/lib.rs
@@ -10,8 +10,8 @@
 #![warn(unused_crate_dependencies)]
 #![allow(elided_lifetimes_in_paths)]
 
-#[cfg(all(feature = "cpu", feature = "metal"))]
-compile_error!("cannot enable `cpu` and `metal` at the same time");
+#[cfg(all(feature = "onnx", feature = "ggml"))]
+compile_error!("cannot enable `onnx` and `ggml` at the same time");
 
 #[cfg(any(bench, test))]
 use criterion as _;
