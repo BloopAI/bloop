764d59731abd9605f44bceaf7f446bbf6070d224
rsdy <p@symmetree.dev>
2023-10-24 16:59:21 +0200

    Create a runner for background workers

--- server/bleep/src/periodic.rs
+++ server/bleep/src/periodic.rs
@@ -6,48 +6,26 @@ pub(crate) use remotes::*;
 
 use crate::Application;
 
+fn single_threaded_executor<F: std::future::Future<Output = ()> + Send>(
+    app: &Application,
+    f: impl (FnOnce(Application) -> F) + Send + 'static,
+) {
+    let app = app.clone();
+    std::thread::spawn(move || {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .expect("failed to start background jobs")
+            .block_on((f)(app));
+    });
+}
+
 pub(crate) fn start_background_jobs(app: Application) {
     if !app.env.is_cloud_instance() {
-        let app = app.clone();
-        std::thread::spawn(move || {
-            tokio::runtime::Builder::new_current_thread()
-                .enable_all()
-                .build()
-                .expect("failed to start background jobs")
-                .block_on(clear_disk_logs(app.clone()));
-        });
+        single_threaded_executor(&app, clear_disk_logs);
     }
 
-    {
-        let app = app.clone();
-        std::thread::spawn(move || {
-            tokio::runtime::Builder::new_current_thread()
-                .enable_all()
-                .build()
-                .expect("failed to start background jobs")
-                .block_on(sync_github_status(app.clone()));
-        });
-    }
-
-    {
-        let app = app.clone();
-        std::thread::spawn(move || {
-            tokio::runtime::Builder::new_current_thread()
-                .enable_all()
-                .build()
-                .expect("failed to start background jobs")
-                .block_on(check_repo_updates(app.clone()));
-        });
-    }
-
-    {
-        let app = app.clone();
-        std::thread::spawn(move || {
-            tokio::runtime::Builder::new_current_thread()
-                .enable_all()
-                .build()
-                .expect("failed to start background jobs")
-                .block_on(log_and_branch_rotate(app.clone()));
-        });
-    }
+    single_threaded_executor(&app, sync_github_status);
+    single_threaded_executor(&app, check_repo_updates);
+    single_threaded_executor(&app, log_and_branch_rotate);
 }
