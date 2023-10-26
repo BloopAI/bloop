8aec3c5443d8b5eaeab00558e4d3d184666805bd
calyptobai <calyptobai@gmail.com>
2023-10-26 10:06:14 -0400

    Revert "bump tantivy to v0.21 (#1043)"
    This reverts commit bd480b6176b291b9c920b55f52afb9677170985a.

--- server/bleep/src/query/ranking.rs
+++ server/bleep/src/query/ranking.rs
@@ -1,37 +1,32 @@
-use std::time::SystemTime;
+use std::{sync::Arc, time::SystemTime};
 
 use tantivy::{
     collector::{ScoreSegmentTweaker, ScoreTweaker},
-    fastfield::Column,
+    fastfield::{BytesFastFieldReader, Column},
     DocId, Score,
 };
-use tantivy_columnar::{column_values::ColumnValues, BytesColumn};
 
 use crate::indexes::file::File;
 
 pub struct DocumentTweaker(pub File);
 pub struct SegmentScorer {
-    line_length: Column<f64>,
-    lang: BytesColumn,
-    last_commit: Column<u64>,
+    line_length: Arc<dyn Column<f64>>,
+    lang: BytesFastFieldReader,
+    last_commit: Arc<dyn Column<u64>>,
 }
 
 impl ScoreSegmentTweaker<Score> for SegmentScorer {
     fn score(&mut self, doc: DocId, mut score: Score) -> Score {
         // * 1000 if it's a language we understand
-        let mut bytes = Vec::new();
-        self.lang.ords().values_for_doc(doc).for_each(|ord| {
-            self.lang.ord_to_bytes(ord, &mut bytes).unwrap();
-        });
-        score *= 1.0 + bytes.len().min(1) as f32 * 999.0;
+        score *= 1.0 + self.lang.num_bytes(doc).min(1) as f32 * 999.0;
 
         // Penalty for lines that are too long
-        score /= self.line_length.values.get_val(doc).clamp(20.0, 1000.0) as f32;
+        score /= self.line_length.get_val(doc).clamp(20.0, 1000.0) as f32;
         score /= SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap()
             .as_secs()
-            .saturating_sub(self.last_commit.values.get_val(doc))
+            .saturating_sub(self.last_commit.get_val(doc))
             .min(5_000_000) as f32;
 
         score
@@ -45,17 +40,13 @@ impl ScoreTweaker<Score> for DocumentTweaker {
         &self,
         segment_reader: &tantivy::SegmentReader,
     ) -> tantivy::Result<Self::Child> {
-        let Self(file) = self;
-        let schema = file.schema();
-        let avg_line_length_field = schema.get_field_name(file.avg_line_length);
-        let lang_field = schema.get_field_name(file.lang);
-        let last_commit_unix_seconds_field = schema.get_field_name(file.last_commit_unix_seconds);
+        let Self(schema) = self;
         Ok(SegmentScorer {
-            line_length: segment_reader.fast_fields().f64(avg_line_length_field)?,
-            lang: segment_reader.fast_fields().bytes(lang_field)?.unwrap(),
+            line_length: segment_reader.fast_fields().f64(schema.avg_line_length)?,
+            lang: segment_reader.fast_fields().bytes(schema.lang)?,
             last_commit: segment_reader
                 .fast_fields()
-                .u64(last_commit_unix_seconds_field)?,
+                .u64(schema.last_commit_unix_seconds)?,
         })
     }
 }
