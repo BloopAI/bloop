77ae257c1ffb424a0f329ed57741505b45f45dac
calyptobai <calyptobai@gmail.com>
2023-11-24 20:47:38 -0500

    WIP: projects
    Back-end API changes include:
    
    - Addition of `projects` table
      - Studios now live *inside* a project
      - Ownership was moved from studios to projects
    - New routes:
      - `GET /api/projects`: returns a list of:
    
        `[ { id: number, name: string, modified_at: date string } ]`
    
      - `POST /api/projects/`: takes in a body like:
    
        `{ name: string | null }`
    
        Note: there is a default name generated here if not provided, "New Project"
    
        This route returns a string body which is the ID
    
      - `GET /api/projects/:id`: if the project exists, returns:
    
        `{ name: string }`
    
      - `POST /api/projects/:id`: updates a project, takes a body of:
    
        `{ name: string }`
    
        Returns nothing
    
    - Additionally, all `/api/studio/...` routes have been moved to
      `/projects/:id/studios/..`
      - Note: `studio` was changed to `studios`
      - Note: all routes remain otherwise unchanged

--- server/bleep/migrations/20231122012638_projects.sql
+++ server/bleep/migrations/20231122012638_projects.sql
@@ -1,7 +1,9 @@
 CREATE TABLE projects (
     id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     name TEXT
 );
 
 ALTER TABLE studios ADD COLUMN project_id INTEGER NOT NULL;
+ALTER TABLE studios DROP COLUMN user_id;
 ALTER TABLE conversations ADD COLUMN project_id INTEGER NOT NULL;
---
+++ server/bleep/src/webserver/project.rs
@@ -0,0 +1,135 @@
+use crate::{webserver, Application};
+use axum::{
+    extract::{Path, Query},
+    Extension, Json,
+};
+use chrono::NaiveDateTime;
+
+use super::{middleware::User, Error};
+
+fn default_name() -> String {
+    "New Project".into()
+}
+
+#[derive(serde::Serialize)]
+pub struct ListItem {
+    id: i64,
+    name: String,
+    modified_at: Option<NaiveDateTime>,
+}
+
+pub async fn list(
+    app: Extension<Application>,
+    user: Extension<User>,
+) -> webserver::Result<Json<Vec<ListItem>>> {
+    let user_id = user.username().ok_or_else(super::no_user_id)?.to_string();
+
+    let projects = sqlx::query! {
+        "SELECT p.id, p.name, (
+            SELECT ss.modified_at
+            FROM studio_snapshots ss
+            JOIN studios s ON s.project_id = p.id AND ss.studio_id = s.id
+            ORDER BY ss.modified_at DESC
+            LIMIT 1
+        ) AS modified_at
+        FROM projects p
+        WHERE user_id = ?",
+        user_id,
+    }
+    .fetch_all(&*app.sql)
+    .await?
+    .into_iter()
+    .map(|row| ListItem {
+        id: row.id,
+        name: row.name.unwrap_or_else(default_name),
+        modified_at: row.modified_at,
+    })
+    .collect();
+
+    Ok(Json(projects))
+}
+
+#[derive(serde::Deserialize)]
+pub struct Create {
+    name: Option<String>,
+}
+
+pub async fn create(
+    app: Extension<Application>,
+    user: Extension<User>,
+    Query(params): Query<Create>,
+) -> webserver::Result<String> {
+    let user_id = user.username().ok_or_else(super::no_user_id)?.to_string();
+
+    let project_id = sqlx::query! {
+        "INSERT INTO projects (user_id, name) VALUES (?, ?) RETURNING id",
+        user_id,
+        params.name,
+    }
+    .fetch_one(&*app.sql)
+    .await?
+    .id;
+
+    Ok(project_id.to_string())
+}
+
+#[derive(serde::Serialize)]
+pub struct Get {
+    name: String,
+}
+
+pub async fn get(
+    app: Extension<Application>,
+    user: Extension<User>,
+    Path(id): Path<i64>,
+) -> webserver::Result<Json<Vec<Get>>> {
+    let user_id = user.username().ok_or_else(super::no_user_id)?.to_string();
+
+    let projects = sqlx::query! {
+        "SELECT name, (
+            SELECT ss.modified_at
+            FROM studio_snapshots ss
+            JOIN studios s ON s.project_id = $1 AND ss.studio_id = s.id
+            ORDER BY ss.modified_at DESC
+            LIMIT 1
+        ) AS modified_at
+        FROM projects
+        WHERE id = $1 AND user_id = $2",
+        id,
+        user_id,
+    }
+    .fetch_all(&*app.sql)
+    .await?
+    .into_iter()
+    .map(|row| Get {
+        name: row.name.unwrap_or_else(default_name),
+    })
+    .collect();
+
+    Ok(Json(projects))
+}
+
+#[derive(serde::Deserialize)]
+pub struct Update {
+    name: String,
+}
+
+pub async fn update(
+    app: Extension<Application>,
+    user: Extension<User>,
+    Path(id): Path<i64>,
+    Json(update): Json<Update>,
+) -> webserver::Result<()> {
+    let user_id = user.username().ok_or_else(super::no_user_id)?.to_string();
+
+    sqlx::query! {
+        "UPDATE projects SET name = ? WHERE id = ? AND user_id = ? RETURNING id",
+        update.name,
+        id,
+        user_id,
+    }
+    .fetch_one(&*app.sql)
+    .await
+    .map(|_id| ())
+    .map_err(Error::internal)
+}
