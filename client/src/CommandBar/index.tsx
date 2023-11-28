3a97025b7ae4978d2146c5418789a8da6ffce988
anastasiia <anastasiya1155@gmail.com>
2023-11-28 11:49:40 -0500

    clean up

--- client/src/CommandBar/index.tsx
+++ client/src/CommandBar/index.tsx
@@ -1,15 +1,8 @@
 import { memo, useCallback, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import Modal from '../components/Modal';
 import useKeyboardNavigation from '../hooks/useKeyboardNavigation';
-import {
-  CommandBarActiveStepType,
-  CommandBarItemType,
-  CommandBarStepEnum,
-  CommandBarStepType,
-} from '../types/general';
+import { CommandBarStepEnum } from '../types/general';
 import CommandBarContextProvider from '../context/providers/CommandBarContextProvider';
-import { getContextItems, getProjectItems } from './items';
 import Initial from './Initial';
 import PrivateRepos from './PrivateRepos';
 import PublicRepos from './PublicRepos';
@@ -19,50 +12,18 @@ import Documentation from './Documentation';
 type Props = {};
 
 const CommandBar = ({}: Props) => {
-  const { t } = useTranslation();
   const [isVisible, setIsVisible] = useState(false);
-  const [focusedItem, setFocusedItem] = useState<CommandBarItemType | null>(
-    null,
-  );
   const [chosenStep, setChosenStep] = useState<{
     id: CommandBarStepEnum;
     data?: Record<string, any>;
   }>({
     id: CommandBarStepEnum.INITIAL,
   });
-  const [activeStepShort, setActiveStepShort] = useState<CommandBarStepType>({
-    id: 'initial',
-    label: '',
-  });
-  const [activeStepFull, setActiveStepFull] =
-    useState<CommandBarActiveStepType>({
-      parents: [],
-      onBack: () => {},
-      sections: [
-        {
-          items: getContextItems(t),
-          itemsOffset: 0,
-          label: t('Manage context'),
-        },
-        {
-          items: getProjectItems(t),
-          itemsOffset: getContextItems(t).length,
-          label: t('Recent projects'),
-        },
-      ],
-    });
 
   const handleClose = useCallback(() => {
     setIsVisible(false);
-  }, []);
-
-  const handleBack = useCallback(() => {
-    setActiveStepShort((prev) => {
-      if (!prev.parent) {
-        setIsVisible(false);
-        return prev;
-      }
-      return prev.parent;
+    setChosenStep({
+      id: CommandBarStepEnum.INITIAL,
     });
   }, []);
 
