8049e7d138c80d610ce7f37c041ee57d212076d0
calyptobai <calyptobai@gmail.com>
2023-12-06 17:02:23 -0500

    WIP: Projects
    Add constraints and foreign keys on new project models

--- server/bleep/migrations/20231122012638_projects.sql
+++ server/bleep/migrations/20231122012638_projects.sql
@@ -4,15 +4,16 @@ CREATE TABLE projects (
     name TEXT
 );
 
-ALTER TABLE studios ADD COLUMN project_id INTEGER NOT NULL;
+ALTER TABLE studios ADD COLUMN project_id INTEGER NOT NULL REFERENCES projects (id) ON DELETE CASCADE;
 ALTER TABLE studios DROP COLUMN user_id;
-ALTER TABLE conversations ADD COLUMN project_id INTEGER NOT NULL;
+ALTER TABLE conversations ADD COLUMN project_id INTEGER NOT NULL REFERENCES projects (id) ON DELETE CASCADE;
 ALTER TABLE conversations DROP COLUMN repo_ref;
 ALTER TABLE conversations DROP COLUMN user_id;
 
 CREATE TABLE project_repos (
     id INTEGER PRIMARY KEY,
-    project_id INTEGER NOT NULL,
+    project_id INTEGER NOT NULL REFERENCES projects (id) ON DELETE CASCADE,
     repo_ref TEXT NOT NULL,
-    branch TEXT
+    branch TEXT,
+    UNIQUE (project_id, repo_ref)
 );
